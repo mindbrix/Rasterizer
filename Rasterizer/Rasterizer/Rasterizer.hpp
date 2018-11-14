64c3b66737b003a847655206a0d722f221e9a12d
Nigel Barber <nigel@mindbrix.co.uk>
2018-11-14 00:06:01 +0000

    radixSort() - remove template

--- Rasterizer/Rasterizer/Rasterizer.hpp
+++ Rasterizer/Rasterizer/Rasterizer.hpp
@@ -778,15 +778,14 @@ struct Rasterizer {
             *dst++ += *src++;
 #endif
     }
-    template<typename T, typename C>
-    static void radixSort(T *in, int n, C *counts0, C *counts1, T *out) {
-        T x;
-        memset(counts0, 0, sizeof(C) * 256);
+    static void radixSort(uint32_t *in, int n, short *counts0, short *counts1, uint32_t *out) {
+        uint32_t x;
+        memset(counts0, 0, sizeof(short) * 256);
         int i;
         for (i = 0; i < n; i++)
             counts0[in[i] & 0xFF]++;
         prefixSum(counts0, 256);
-        memset(counts1, 0, sizeof(C) * 256);
+        memset(counts1, 0, sizeof(short) * 256);
         for (i = n - 1; i >= 0; i--) {
             x = in[i];
             out[--counts0[x & 0xFF]] = x;
