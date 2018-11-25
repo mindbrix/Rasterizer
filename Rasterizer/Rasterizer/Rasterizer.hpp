413d5bcfdd53f6037f1c694902422165b3bf69d2
Nigel Barber <nigel@mindbrix.co.uk>
2018-11-25 12:50:18 +0000

    Bitmap::Bitmap() - formatting

--- Rasterizer/Rasterizer/Rasterizer.hpp
+++ Rasterizer/Rasterizer/Rasterizer.hpp
@@ -124,7 +124,8 @@ struct Rasterizer {
     };
     struct Bitmap {
         Bitmap() {}
-        Bitmap(void *data, size_t width, size_t height, size_t rowBytes, size_t bpp) : data((uint8_t *)data), width(width), height(height), rowBytes(rowBytes), bpp(bpp), bytespp(bpp / 8) {}
+        Bitmap(void *data, size_t width, size_t height, size_t rowBytes, size_t bpp)
+            : data((uint8_t *)data), width(width), height(height), rowBytes(rowBytes), bpp(bpp), bytespp(bpp / 8) {}
         inline uint8_t *pixelAddress(short x, short y) { return data + rowBytes * (height - 1 - y) + x * bytespp; }
         uint8_t *data;
         size_t width, height, rowBytes, bpp, bytespp;
