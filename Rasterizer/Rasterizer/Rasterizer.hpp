8010e702b0e54a467d136cbcb510174c6d1a3d8b
Nigel Barber <nigel@mindbrix.co.uk>
2018-12-04 10:08:59 +0000

    writeDeltasToBitmap() - null pointer guard on pixelAddress()

--- Rasterizer/Rasterizer/Rasterizer.hpp
+++ Rasterizer/Rasterizer/Rasterizer.hpp
@@ -528,7 +528,7 @@ struct Rasterizer {
             for (float y = clip.ly; y < clip.uy; y++, deltas += stride)
                 *deltas = 0.f;
         else {
-            uint8_t *pixelAddress = bitmap->pixelAddress(clip.lx, clip.ly), *pixel;
+            uint8_t *pixelAddress = bitmap ? bitmap->pixelAddress(clip.lx, clip.ly) : nullptr, *pixel;
             float src0 = src[0], src1 = src[1], src2 = src[2], srcAlpha = src[3] * 0.003921568627f, y, x, cover, alpha, *delta;
             for (y = clip.ly; y < clip.uy; y++, deltas += stride, pixelAddress -= bitmap->stride, *delta = 0.f)
                 for (cover = alpha = 0.f, delta = deltas, pixel = pixelAddress, x = clip.lx; x < clip.ux; x++, delta++, pixel += bitmap->bytespp) {
