616959225f3fc5e3267bd81a91517f3ad3008bad
Nigel Barber <nigel@mindbrix.co.uk>
2018-12-13 19:23:22 +0000

    Skeleton MetalLayer

---
+++ Rasterizer/Rasterizer/MetalLayer.h
@@ -0,0 +1,19 @@
+//
+//  MetalLayer.h
+//  Rasterizer
+//
+//  Created by Nigel Barber on 13/12/2018.
+//  Copyright © 2018 @mindbrix. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+#import "RasterizerCoreGraphics.hpp"
+
+@protocol LayerDelegate <NSObject>
+- (void)writeBuffer:(Rasterizer::Buffer *)buffer forLayer:(CALayer *)layer;
+@end
+
+
+@interface MetalLayer : CAMetalLayer
+@property(weak) id <LayerDelegate> layerDelegate;
+@end
---
+++ Rasterizer/Rasterizer/MetalLayer.mm
@@ -0,0 +1,118 @@
+//
+//  MetalLayer.mm
+//  Rasterizer
+//
+//  Created by Nigel Barber on 13/12/2018.
+//  Copyright © 2018 @mindbrix. All rights reserved.
+//
+
+#import "MetalLayer.h"
+#import <Metal/Metal.h>
+
+@interface MetalLayer ()
+{
+    Rasterizer::Buffer _buffer0;
+    Rasterizer::Buffer _buffer1;
+}
+@property (nonatomic) dispatch_semaphore_t inflight_semaphore;
+@property (nonatomic) id <MTLCommandQueue> commandQueue;
+@property (nonatomic) id <MTLLibrary> defaultLibrary;
+@property (nonatomic) BOOL odd;
+@property (nonatomic) id <MTLBuffer> mtlBuffer0;
+@property (nonatomic) id <MTLBuffer> mtlBuffer1;
+@property (nonatomic) id <MTLRenderPipelineState> edgesPipelineState;
+@property (nonatomic) id <MTLRenderPipelineState> quadsPipelineState;
+@property (nonatomic) id <MTLRenderPipelineState> opaquesPipelineState;
+@property (nonatomic) id <MTLDepthStencilState> depthState;
+@property (nonatomic) id <MTLTexture> depthTexture;
+@property (nonatomic) id <MTLTexture> accumulationTexture;
+
+@end
+
+
+@implementation MetalLayer
+
+- (id)init {
+    self = [super init];
+    if (!self)
+        return nil;
+    
+    self.opaque = NO;
+    self.needsDisplayOnBoundsChange = YES;
+    self.actions = @{ @"onOrderIn": [NSNull null],
+                      @"onOrderOut": [NSNull null],
+                      @"sublayers": [NSNull null],
+                      @"contents": [NSNull null],
+                      @"backgroundColor": [NSNull null],
+                      @"bounds": [NSNull null] };
+    self.device = MTLCreateSystemDefaultDevice();
+    self.pixelFormat = MTLPixelFormatBGRA8Unorm;
+    self.magnificationFilter = kCAFilterNearest;
+    CGColorSpaceRef rgb = CGColorSpaceCreateDeviceRGB();
+    self.colorspace = rgb;
+    CGColorSpaceRelease(rgb);
+    
+    self.commandQueue = [self.device newCommandQueue];
+    self.defaultLibrary = [self.device newDefaultLibrary];
+    self.inflight_semaphore = dispatch_semaphore_create(1);
+    
+    MTLDepthStencilDescriptor *depthStencilDescriptor = [MTLDepthStencilDescriptor new];
+    depthStencilDescriptor.depthWriteEnabled = YES;
+    depthStencilDescriptor.depthCompareFunction = MTLCompareFunctionGreaterEqual;
+    self.depthState = [self.device newDepthStencilStateWithDescriptor:depthStencilDescriptor];
+    
+    return self;
+}
+
+- (void)display {
+    @autoreleasepool {
+        self.drawableSize = CGSizeMake(ceil(self.bounds.size.width * self.contentsScale), ceil(self.bounds.size.height * self.contentsScale));
+        dispatch_semaphore_wait(_inflight_semaphore, DISPATCH_TIME_FOREVER);
+        [self draw];
+    }
+}
+
+- (void)draw {
+    Rasterizer::Buffer *buffer = _odd ? & _buffer1 : & _buffer0;
+    _odd = !_odd;
+    if ([self.layerDelegate respondsToSelector:@selector(writeBuffer:forLayer:)])
+        [self.layerDelegate writeBuffer:buffer forLayer:self];
+    
+    id <CAMetalDrawable> drawable = [self nextDrawable];
+    if (drawable == nil) {
+        dispatch_semaphore_signal(_inflight_semaphore);
+        return;
+    }
+    if (self.drawableSize.width != self.depthTexture.width || self.drawableSize.height != self.depthTexture.height) {
+        MTLTextureDescriptor* desc = [MTLTextureDescriptor texture2DDescriptorWithPixelFormat:MTLPixelFormatDepth32Float
+                                                                                        width:self.drawableSize.width
+                                                                                       height:self.drawableSize.height
+                                                                                    mipmapped:NO];
+        desc.storageMode = MTLStorageModePrivate;
+        desc.usage = MTLTextureUsageRenderTarget;
+        self.depthTexture = [self.device newTextureWithDescriptor:desc];
+        [self.depthTexture setLabel:@"depthTexture"];
+        
+        desc.width = self.drawableSize.width * 4;
+        desc.height = self.drawableSize.height / 4;
+        desc.usage = MTLTextureUsageRenderTarget | MTLTextureUsageShaderRead;
+        desc.pixelFormat = MTLPixelFormatR32Float;
+        self.accumulationTexture = [self.device newTextureWithDescriptor:desc];
+        [self.accumulationTexture setLabel:@"accumulationTexture"];
+    }
+    id<MTLCommandBuffer> commandBuffer = [self.commandQueue commandBuffer];
+    
+    for (size_t i = 0; i < buffer->entries.end; i++) {
+        switch (buffer->entries.base[i].type) {
+            default:
+                break;
+        }
+    }
+    __block dispatch_semaphore_t block_sema = _inflight_semaphore;
+    [commandBuffer addCompletedHandler:^(id<MTLCommandBuffer> buffer) {
+        dispatch_semaphore_signal(block_sema);
+    }];
+    [commandBuffer presentDrawable:drawable];
+    [commandBuffer commit];
+}
+@end
