dff54047e0edfcef1054cae0e77c5a0775f88105
Nigel Barber <nigel@mindbrix.co.uk>
2018-12-13 17:43:55 +0000

    writeContextsToBuffer() - optimise segments addressing

--- Rasterizer/Rasterizer/Rasterizer.hpp
+++ Rasterizer/Rasterizer/Rasterizer.hpp
@@ -279,20 +279,19 @@ struct Rasterizer {
                         end += (eend - context.gpu.edges.idx) * sizeof(Buffer::Edge);
                         GPU::Edge *src = (GPU::Edge *)(context.gpu.edges.base + context.gpu.edges.idx);
                         Buffer::Edge *dst = (Buffer::Edge *)(buffer.data.base + begin);
-                        Row<Segment> *segments;
+                        Segment *segments;
                         for (j = context.gpu.edges.idx; j < eend; j++, src++, dst++) {
                             dst->cover = src->cover;
                             dst->quad = src->quad;
-                            segments = & context.segments[src->index.iy];
+                            segments = context.segments[src->index.iy].base + src->index.idx;
                             for (k = 0; k < 4; k++) {
                                 if (src->index.is[k] == 0xFFFF)
                                     new (& dst->segments[k]) Segment(0.f, 0.f, 0.f, 0.f);
                                 else
-                                    dst->segments[k] = segments->base[src->index.idx + src->index.is[k]];
+                                    dst->segments[k] = segments[src->index.is[k]];
                             }
                         }
                         new (buffer.entries.alloc(1)) Buffer::Entry(Buffer::Entry::kEdges, begin, end);
-                        
                         context.gpu.edges.idx = eend;
                         begin = end;
                     }
