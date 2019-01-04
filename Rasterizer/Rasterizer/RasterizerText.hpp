983cef927eae7466dfdbfa5d319a4e35e7df34e9
Nigel Barber <nigel@mindbrix.co.uk>
2019-01-04 12:55:48 +0000

    Font - font --> info

--- Rasterizer/Rasterizer/RasterizerText.hpp
+++ Rasterizer/Rasterizer/RasterizerText.hpp
@@ -13,15 +13,15 @@
 
 struct RasterizerText {
     struct Font {
-        Font() { bzero(& font, sizeof(font)); }
+        Font() { bzero(& info, sizeof(info)); }
         int init(const void *bytes) {
             const unsigned char *ttf_buffer = (const unsigned char *)bytes;
             int offset = stbtt_GetFontOffsetForIndex(ttf_buffer, 0);
             if (offset == -1)
                 return 0;
-            return stbtt_InitFont(& font, ttf_buffer, offset);
+            return stbtt_InitFont(& info, ttf_buffer, offset);
         }
-        stbtt_fontinfo font;
+        stbtt_fontinfo info;
     };
     
     static void writeGlyphGrid(NSString *fontName, RasterizerCoreGraphics::Scene& scene) {
@@ -34,12 +34,12 @@ struct RasterizerText {
         int flx, fly, fux, fuy, fw, fh, fdim, fsize = 32, d, ix, iy;
         RasterizerText::Font font;
         if (font.init(data.bytes) != 0) {
-            stbtt_GetFontBoundingBox(& font.font, & flx, & fly, & fux, & fuy);
+            stbtt_GetFontBoundingBox(& font.info, & flx, & fly, & fux, & fuy);
             fw = fux - flx, fh = fuy - fly, fdim = fw < fh ? fw : fh;
-            d = ceilf(sqrtf((float)font.font.numGlyphs));
+            d = ceilf(sqrtf((float)font.info.numGlyphs));
             float s = float(fsize) / float(fdim);
-            for (int glyph = 0; glyph < font.font.numGlyphs; glyph++)
-                if (stbtt_IsGlyphEmpty(& font.font, glyph) == 0) {
+            for (int glyph = 0; glyph < font.info.numGlyphs; glyph++)
+                if (stbtt_IsGlyphEmpty(& font.info, glyph) == 0) {
                     ix = glyph % d, iy = glyph / d;
                     Rasterizer::AffineTransform CTM = { s, 0, 0, s, s * float(ix * fdim), s * float(iy * fdim) };
                     scene.bgras.emplace_back(*((uint32_t *)bgra));
@@ -53,7 +53,7 @@ struct RasterizerText {
     static void writeGlyphPath(Font& font, int glyph, Rasterizer::Path& path) {
         stbtt_vertex *vertices, *vertex;
         int nverts, i;
-        nverts = stbtt_GetGlyphShape(& font.font, glyph, & vertices);
+        nverts = stbtt_GetGlyphShape(& font.info, glyph, & vertices);
         if (nverts) {
             for (vertex = vertices, i = 0; i < nverts; i++, vertex++) {
                 switch (vertex->type) {
@@ -71,7 +71,7 @@ struct RasterizerText {
                         break;
                 }
             }
-            stbtt_FreeShape(& font.font, vertices);
+            stbtt_FreeShape(& font.info, vertices);
         }
     }
 };
