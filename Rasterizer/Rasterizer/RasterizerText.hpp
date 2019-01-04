3a41d9b955c629ae98707e89602fa85d378495ba
Nigel Barber <nigel@mindbrix.co.uk>
2019-01-04 14:02:30 +0000

    Split writeGlyphGrid()

--- Rasterizer/Rasterizer/RasterizerText.hpp
+++ Rasterizer/Rasterizer/RasterizerText.hpp
@@ -23,24 +23,26 @@ struct RasterizerText {
         }
         stbtt_fontinfo info;
     };
-    
+	
+	static void writeGlyphGrid(Font& font, RasterizerCoreGraphics::Scene& scene) {
+		uint8_t bgra[4] = { 0, 0, 0, 255 };
+		int flx, fly, fux, fuy, fw, fh, fdim, fsize = 32, d;
+		stbtt_GetFontBoundingBox(& font.info, & flx, & fly, & fux, & fuy);
+		fw = fux - flx, fh = fuy - fly, fdim = fw < fh ? fw : fh;
+		d = ceilf(sqrtf((float)font.info.numGlyphs));
+		float s = float(fsize) / float(fdim);
+		for (int glyph = 0; glyph < font.info.numGlyphs; glyph++)
+		if (stbtt_IsGlyphEmpty(& font.info, glyph) == 0) {
+			scene.bgras.emplace_back(*((uint32_t *)bgra));
+			scene.paths.emplace_back();
+			writeGlyphPath(font, glyph, scene.paths.back());
+			scene.ctms.emplace_back(s, 0, 0, s, s * float(glyph % d * fdim), s * float(glyph / d * fdim));
+		}
+	}
     static void writeGlyphGrid(const void *bytes, RasterizerCoreGraphics::Scene& scene) {
-        uint8_t bgra[4] = { 0, 0, 0, 255 };
-        int flx, fly, fux, fuy, fw, fh, fdim, fsize = 32, d;
         RasterizerText::Font font;
-        if (font.init(bytes) != 0) {
-            stbtt_GetFontBoundingBox(& font.info, & flx, & fly, & fux, & fuy);
-            fw = fux - flx, fh = fuy - fly, fdim = fw < fh ? fw : fh;
-            d = ceilf(sqrtf((float)font.info.numGlyphs));
-            float s = float(fsize) / float(fdim);
-            for (int glyph = 0; glyph < font.info.numGlyphs; glyph++)
-                if (stbtt_IsGlyphEmpty(& font.info, glyph) == 0) {
-                    scene.bgras.emplace_back(*((uint32_t *)bgra));
-                    scene.paths.emplace_back();
-                    writeGlyphPath(font, glyph, scene.paths.back());
-                    scene.ctms.emplace_back(s, 0, 0, s, s * float(glyph % d * fdim), s * float(glyph / d * fdim));
-                }
-        }
+        if (font.init(bytes) != 0)
+			writeGlyphGrid(font, scene);
     }
     
     static void writeGlyphPath(Font& font, int glyph, Rasterizer::Path& path) {
