8b45eca3310d0d444b616789007934ef50c32f87
Nigel Barber <nigel@mindbrix.co.uk>
2019-01-05 13:16:55 +0000

    Add test text

--- Rasterizer/Rasterizer/RasterizerView.mm
+++ Rasterizer/Rasterizer/RasterizerView.mm
@@ -87,10 +87,12 @@
     _testScene.cgscene.empty();
 	RasterizerText::Font font;
 	uint8_t bgra[4] = { 0, 0, 0, 255 };
+	const char *text = "One of the obvious motivations has been Objective-C, and in particular the notion of Software-ICs, components that are connected via dynamic messages. This is actually an instance of the Scripted Components (pdf) pattern, with the interesting twist that unlike most of the instances of this pattern, there is only a single programming language for both the scripts and the components, that language being Objective-C.";
+	
     const char *label = "Hello, world!\nABCDEFGHIJKLMNOPQRSTUVWXYZ\nabcdefghijklmnopqrstuvwxyz 0123456789\n!@$%^&*()-_=+[]{};:\'\"\\|~,.<>/?";
 	if (font.init(data.bytes, nsFont.fontName.UTF8String) != 0) {
-//        RasterizerText::writeGlyphs(font, float(nsFont.pointSize), bgra, label, _testScene.scene);
-        RasterizerText::writeGlyphGrid(font, float(nsFont.pointSize), bgra, _testScene.scene);
+        RasterizerText::writeGlyphs(font, float(nsFont.pointSize), bgra, text, _testScene.scene);
+//        RasterizerText::writeGlyphGrid(font, float(nsFont.pointSize), bgra, _testScene.scene);
 		RasterizerCoreGraphics::writeSceneToCGScene(_testScene.scene, _testScene.cgscene);
 	}
 }
