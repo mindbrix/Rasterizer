ff46356bb7013f9876063a9a9710372ae63b5916
Nigel Barber <nigel@mindbrix.co.uk>
2019-01-11 19:04:14 +0000

    writePixel() - use __builtin_convertvector bidirectionally

--- Rasterizer/Rasterizer/Rasterizer.hpp
+++ Rasterizer/Rasterizer/Rasterizer.hpp
@@ -953,12 +953,12 @@ struct Rasterizer {
     }
     static inline void writePixel(float src0, float src1, float src2, float alpha, uint8_t *dst) {
 #ifdef RASTERIZER_SIMD
-        vec4f src4 = { src0, src1, src2, 255.f }, alpha4 = { alpha, alpha, alpha, alpha }, mul4 = alpha4 * src4, dst4, one4 = { 1.f, 1.f, 1.f, 1.f };
+        vec4f src4 = { src0, src1, src2, 255.f }, alpha4 = { alpha, alpha, alpha, alpha }, mul4 = alpha4 * src4, one4 = { 1.f, 1.f, 1.f, 1.f };
         if (*((uint32_t *)dst)) {
-            dst4 = { float(dst[0]), float(dst[1]), float(dst[2]), float(dst[3]) };
-            mul4 += dst4 * (one4 - alpha4);
+            vec4b bgra = { dst[0], dst[1], dst[2], dst[3] };
+            mul4 += __builtin_convertvector(bgra, vec4f) * (one4 - alpha4);
         }
-        *((vec4b *)dst) = __builtin_convertvector (mul4, vec4b);
+        *((vec4b *)dst) = __builtin_convertvector(mul4, vec4b);
 #else
         if (*((uint32_t *)dst) == 0)
             dst[0] = src0 * alpha, dst[1] = src1 * alpha, dst[2] = src2 * alpha, dst[3] = 255.f * alpha;
