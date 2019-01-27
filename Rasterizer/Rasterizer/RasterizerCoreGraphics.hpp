54aab814333c7535501f592d93863baab8b1e4bf
Nigel Barber <nigel@mindbrix.co.uk>
2019-01-27 09:55:48 +0000

    RasterizerCoreGraphics::createStrokedPath() - remove false simplification

--- Rasterizer/Rasterizer/RasterizerCoreGraphics.hpp
+++ Rasterizer/Rasterizer/RasterizerCoreGraphics.hpp
@@ -176,8 +176,10 @@ struct RasterizerCoreGraphics {
         CGFloat scale = 10;
         CGAffineTransform scaleUp = { scale, 0, 0, scale, 0, 0 };
         CGAffineTransform scaleDown = { 1.0 / scale, 0, 0, 1.0 / scale, 0, 0 };
-        CGPathRef stroked = CGPathCreateCopyByStrokingPath(path, & scaleUp, width, cap, join, limit);
+        CGPathRef scaledUp = CGPathCreateCopyByTransformingPath(path, & scaleUp);
+        CGPathRef stroked = CGPathCreateCopyByStrokingPath(scaledUp, NULL, width * scale, cap, join, limit);
         CGPathRef scaledDown = CGPathCreateCopyByTransformingPath(stroked, & scaleDown);
+        CGPathRelease(scaledUp);
         CGPathRelease(stroked);
         return scaledDown;
     }
