fa64831171401fdcd71d52b2d91331fa32e08544
Nigel Barber <nigel@mindbrix.co.uk>
2019-02-02 21:23:44 +0000

    writeCubic() - remove a < 16 px case

--- Rasterizer/Rasterizer/Rasterizer.hpp
+++ Rasterizer/Rasterizer/Rasterizer.hpp
@@ -739,27 +739,18 @@ struct Rasterizer {
         }
     }
     static void writeCubic(float x0, float y0, float x1, float y1, float x2, float y2, float x3, float y3, float *deltas, uint32_t stride, Row<Segment> *segments) {
-        float s, t, a, count, px0, py0, px1, py1, dt;
-        s = fabsf(x3 - x0 + 3.f * (x1 - x2)) + fabsf(3.f * (x2 + x0) - 6.f * x1);
-        t = fabsf(y3 - y0 + 3.f * (y1 - y2)) + fabsf(3.f * (y2 + y0) - 6.f * y1);
-        a = s * s + t * t;
+        float ax, bx, ay, by, s, t, a, count, px0, py0, px1, py1, dt;
+        ax = x3 - x0 + 3.f * (x1 - x2), bx = 3.f * (x2 + x0) - 6.f * x1;
+        ay = y3 - y0 + 3.f * (y1 - y2), by = 3.f * (y2 + y0) - 6.f * y1;
+        s = fabsf(ax) + fabsf(bx), t = fabsf(ay) + fabsf(by), a = s * s + t * t;
         if (a < 0.1f)
             writeLine(x0, y0, x3, y3, deltas, stride, segments);
-        else if (a < 16.f) {
-            const float w0 = 8.0 / 27.0, w1 = 4.0 / 9.0, w2 = 2.0 / 9.0, w3 = 1.0 / 27.0;
-            px0 = x0 * w0 + x1 * w1 + x2 * w2 + x3 * w3, py0 = y0 * w0 + y1 * w1 + y2 * w2 + y3 * w3;
-            px1 = x0 * w3 + x1 * w2 + x2 * w1 + x3 * w0, py1 = y0 * w3 + y1 * w2 + y2 * w1 + y3 * w0;
-            writeLine(x0, y0, px0, py0, deltas, stride, segments);
-            writeLine(px0, py0, px1, py1, deltas, stride, segments);
-            writeLine(px1, py1, x3, y3, deltas, stride, segments);
-        } else {
-            count = 4.f + floorf(sqrtf(sqrtf(a - 16.f))), dt = 1.f / count, t = 0.f, px0 = px1 = x0, py0 = py1 = y0;
-            float ax, bx, cx, ay, by, cy, h2, h3, f3x, f2x, f1x, f3y, f2y, f1y;
-            ax = x3 - x0 + 3.f * (x1 - x2), bx = 3.f * (x2 + x0) - 6.f * x1, cx = 3.f * (x1 - x0);
-            ay = y3 - y0 + 3.f * (y1 - y2), by = 3.f * (y2 + y0) - 6.f * y1, cy = 3.f * (y1 - y0);
+        else {
+            count = a < 16.f ? 3.f : 4.f + floorf(sqrtf(sqrtf(a - 16.f))), dt = 1.f / count, px0 = px1 = x0, py0 = py1 = y0;
+            float h2, h3, f3x, f2x, f1x, f3y, f2y, f1y;
             h2 = dt * dt, h3 = h2 * dt;
-            f3x = 6.f * ax * h3, f2x = f3x + 2.f * bx * h2, f1x = ax * h3 + bx * h2 + cx * dt;
-            f3y = 6.f * ay * h3, f2y = f3y + 2.f * by * h2, f1y = ay * h3 + by * h2 + cy * dt;
+            f3x = 6.f * ax * h3, f2x = f3x + 2.f * bx * h2, f1x = ax * h3 + bx * h2 + 3.f * (x1 - x0) * dt;
+            f3y = 6.f * ay * h3, f2y = f3y + 2.f * by * h2, f1y = ay * h3 + by * h2 + 3.f * (y1 - y0) * dt;
             while (--count) {
                 px1 += f1x, f1x += f2x, f2x += f3x;
                 py1 += f1y, f1y += f2y, f2y += f3y;
