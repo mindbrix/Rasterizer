d7d2dfccb78cd85fca2631b5e93b144f56ed965b
Nigel Barber <nigel@mindbrix.co.uk>
2019-02-07 09:28:25 +0000

    writeCubic() - simplify

--- Rasterizer/Rasterizer/Rasterizer.hpp
+++ Rasterizer/Rasterizer/Rasterizer.hpp
@@ -731,14 +731,14 @@ struct Rasterizer {
         }
     }
     static void writeCubic(float x0, float y0, float x1, float y1, float x2, float y2, float x3, float y3, float *deltas, uint32_t stride, Row<Segment> *segments) {
-        float ax, bx, ay, by, s, t, a, count, dt, dt2, f3x, f2x, f1x, f3y, f2y, f1y, px0, px1, py0, py1;
-        ax = x3 - x0 + 3.f * (x1 - x2), bx = 3.f * (x2 + x0) - 6.f * x1;
-        ay = y3 - y0 + 3.f * (y1 - y2), by = 3.f * (y2 + y0) - 6.f * y1;
+        float cx, bx, ax, cy, by, ay, s, t, a, count, dt, dt2, f3x, f2x, f1x, f3y, f2y, f1y, px0, px1, py0, py1;
+        cx = 3.f * (x1 - x0), bx = 3.f * (x2 - x1) - cx, ax = x3 - x0 - cx - bx;
+        cy = 3.f * (y1 - y0), by = 3.f * (y2 - y1) - cy, ay = y3 - y0 - cy - by;
         s = fabsf(ax) + fabsf(bx), t = fabsf(ay) + fabsf(by), a = s * s + t * t;
         count = a < 0.1f ? 1.f : a < 16.f ? 3.f : 4.f + floorf(sqrtf(sqrtf(a - 16.f)));
         dt = 1.f / count, dt2 = dt * dt, ax = ax * dt2 * dt, ay = ay * dt2 * dt, bx = bx * dt2, by = by * dt2;
-        f3x = 6.f * ax, f2x = f3x + 2.f * bx, f1x = ax + bx + 3.f * (x1 - x0) * dt;
-        f3y = 6.f * ay, f2y = f3y + 2.f * by, f1y = ay + by + 3.f * (y1 - y0) * dt;
+        f3x = 6.f * ax, f2x = f3x + 2.f * bx, f1x = ax + bx + cx * dt;
+        f3y = 6.f * ay, f2y = f3y + 2.f * by, f1y = ay + by + cy * dt;
         px0 = px1 = x0, py0 = py1 = y0;
         while (--count) {
             px1 += f1x, f1x += f2x, f2x += f3x, py1 += f1y, f1y += f2y, f2y += f3y;
