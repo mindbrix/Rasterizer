de5339c5980957c58282652611f66e6ad7865980
Nigel Barber <nigel@mindbrix.co.uk>
2019-02-09 08:35:36 +0000

    Buffer::Entry::Type - kColorants first

--- Rasterizer/Rasterizer/Rasterizer.hpp
+++ Rasterizer/Rasterizer/Rasterizer.hpp
@@ -274,7 +274,7 @@ struct Rasterizer {
     };
     struct Buffer {
         struct Entry {
-            enum Type { kEdges, kColorants, kQuads, kOpaques };
+            enum Type { kColorants, kEdges, kQuads, kOpaques };
             Entry() {}
             Entry(Type type, size_t begin, size_t end) : type(type), begin(begin), end(end) {}
             Type type;
