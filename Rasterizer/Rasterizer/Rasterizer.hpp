119e3be480a7aadefab0b0c261dd6133444ca132
Nigel Barber <nigel@mindbrix.co.uk>
2019-02-19 19:28:36 +0000

    drawPath() - compact cache logic

--- Rasterizer/Rasterizer/Rasterizer.hpp
+++ Rasterizer/Rasterizer/Rasterizer.hpp
@@ -554,21 +554,16 @@ struct Rasterizer {
                         AffineTransform m = { 1.f, 0.f, 0.f, 1.f, 0.f, 0.f };
                         if (path.sequence->hash) {
                             Bounds dev = Bounds(path.sequence->bounds.unit(ctm)).integral();
-                            float dot = ctm.a * ctm.c + ctm.b * ctm.d;
-                            float ratio = (ctm.a * ctm.a + ctm.b * ctm.b) / (ctm.c * ctm.c + ctm.d * ctm.d);
-                            if (dot == 0.f && ratio == 1.f && dev.lx == clip.lx && dev.ly == clip.ly && dev.ux == clip.ux && dev.uy == clip.uy) {
-                                cached = true;
+                            float dot = ctm.a * ctm.c + ctm.b * ctm.d, ab = ctm.a * ctm.a + ctm.b * ctm.b, cd = ctm.c * ctm.c + ctm.d * ctm.d;
+                            if ((cached = dot == 0.f && ab / cd == 1.f && dev.lx == clip.lx && dev.ly == clip.ly && dev.ux == clip.ux && dev.uy == clip.uy)) {
                                 auto it = cache.find(path.sequence->hash);
                                 if (it == cache.end()) {
                                     auto entry = cache.emplace(path.sequence->hash, Entry(ctm));
                                     sgmnts = & entry.first->second.segments;
                                     writePath(path, ctm, clip, writeOutlineSegment, Info(nullptr, 0, sgmnts));
-                                } else {
-                                    if ((ctm.a * ctm.a + ctm.b * ctm.b) / (it->second.ctm.a * it->second.ctm.a + it->second.ctm.b * it->second.ctm.b) == 1.f) {
-                                        sgmnts = & it->second.segments;
-                                        m = ctm.concat(it->second.ctm.invert());
-                                    } else
-                                        cached = false;
+                                } else if ((cached = ab / (it->second.ctm.a * it->second.ctm.a + it->second.ctm.b * it->second.ctm.b) == 1.f)) {
+                                    sgmnts = & it->second.segments;
+                                    m = ctm.concat(it->second.ctm.invert());
                                 }
                             }
                         }
