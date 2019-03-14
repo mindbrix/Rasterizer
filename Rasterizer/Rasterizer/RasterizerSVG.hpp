f3d77f7b7b3f0aee874c9e6e7581bf7b14664e5e
Nigel Barber <nigel@mindbrix.co.uk>
2019-03-14 00:27:07 +0000

    RasterizerSVG - create writePhyllotaxisToScene()

--- Rasterizer/Rasterizer/RasterizerSVG.hpp
+++ Rasterizer/Rasterizer/RasterizerSVG.hpp
@@ -34,6 +34,19 @@ struct RasterizerSVG {
         }
         return p;
     }
+    static void writePhyllotaxisToScene(size_t count, RasterizerScene::Scene& scene) {
+        uint8_t bgra[4] = { 0, 0, 0, 255 };
+        Rasterizer::Path rect;
+        rect.ref->addBounds(Rasterizer::Bounds(0.f, 0.f, 1.f, 1.f));
+        const float sine = 0.675490294261524f, cosine = -0.73736887807832f;
+        float vx = 1.f, vy = 0.f, x, y, s, t;
+        for (int i = 0; i < count; i++) {
+            s = sqrtf(i), t = float(i) / float(count);
+            scene.addPath(rect, Rasterizer::Transform(1.f + t, 0.f, 0.f, 1.f + t, s * vx - 0.5f, s * vy - 0.5f), bgra);
+            x = vx * cosine + vy * -sine, y = vx * sine + vy * cosine;
+            vx = x, vy = y;
+        }
+    }
     static Rasterizer::Path createPhyllotaxisPath(size_t count) {
         Rasterizer::Path shapes;
         shapes.ref->addShapes(count);
@@ -91,6 +104,9 @@ struct RasterizerSVG {
             }
             if (0) {
                 scene.addPath(createPhyllotaxisPath(100000), Rasterizer::Transform(1.f, 0.f, 0.f, 1.f, 0.f, 0.f), bgra);
+            }
+            if (1 ) {
+                writePhyllotaxisToScene(100000, scene);
             }
         }
         free(data);
