2065eeff37f4cd5af85d3ac130d9559eec1df847
Nigel Barber <nigel@mindbrix.co.uk>
2019-03-20 12:28:31 +0000

    Create RasterizerEvent.hpp

---
+++ Rasterizer/Rasterizer/RasterizerEvent.hpp
@@ -0,0 +1,21 @@
+//
+//  RasterizerEvent.hpp
+//  Rasterizer
+//
+//  Created by Nigel Barber on 20/03/2019.
+//  Copyright © 2019 @mindbrix. All rights reserved.
+//
+
+struct RasterizerEvent {
+    struct Event {
+        enum Type { kNull = 0, kMouseMove, kMouseUp, kMouseDown, kKeyDown, kKeyUp, kMagnify, kRotate, kTranslate };
+        
+        Event() {}
+        Event(Type type, float x, float y) : type(type), x(x), y(y), keyCode(0) {}
+        Event(Type type, int keyCode) : type(type), x(0.f), y(0.f), keyCode(keyCode) {}
+        
+        Type type;
+        float x, y;
+        int keyCode;
+    };
+};
