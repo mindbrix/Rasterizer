5864c2bd8cb4446c39653eb8b38df62865b30eb9
Nigel Barber <nigel@mindbrix.co.uk>
2019-03-22 12:36:47 +0000

    Segment - delete default constructor

--- Rasterizer/Rasterizer/Rasterizer.hpp
+++ Rasterizer/Rasterizer/Rasterizer.hpp
@@ -215,7 +215,6 @@ struct Rasterizer {
         T *base = nullptr;
     };
     struct Segment {
-        Segment() {}
         Segment(float x0, float y0, float x1, float y1) : x0(x0), y0(y0), x1(x1), y1(y1) {}
         float x0, y0, x1, y1;
     };
