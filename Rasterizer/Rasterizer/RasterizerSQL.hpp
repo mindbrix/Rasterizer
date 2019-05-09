7f52813f61847311e2bb8f7d91c84e6fa4edc5e1
Nigel Barber <nigel@mindbrix.co.uk>
2019-05-09 18:07:12 +0100

    RasterizerSQL::DB::writeQuery() - while value placeholder glyphs

--- Rasterizer/Rasterizer/RasterizerSQL.hpp
+++ Rasterizer/Rasterizer/RasterizerSQL.hpp
@@ -20,22 +20,27 @@ struct RasterizerSQL {
             sqlite3_close(db);
             db = nullptr;
         }
-        void writeQuery(RasterizerTrueType::Font& font, float size, Rasterizer::Bounds bounds, const char *sql, Rasterizer::Scene& scene) {
+        void writeQuery(RasterizerTrueType::Font& font, float size, Rasterizer::Bounds frame, const char *sql, Rasterizer::Scene& scene) {
             sqlite3_stmt *pStmt;
             const char *zTail;
             int status = sqlite3_prepare(db, sql, (int)strlen(sql), & pStmt, & zTail);
             if (status == SQLITE_OK) {
+                Rasterizer::Bounds glyphs, bounds = frame;
                 int columns = sqlite3_column_count(pStmt);
                 for (int i = 0; i < columns; i++) {
                     const char *name = sqlite3_column_name(pStmt, i);
-                    Rasterizer::Bounds glyphBounds = RasterizerTrueType::writeGlyphs(font, size, Rasterizer::Colorant(0, 0, 0, 255), bounds, false, name, scene);
-                    bounds.lx = glyphBounds.ux + 0.333f * size;
+                    glyphs = RasterizerTrueType::writeGlyphs(font, size, Rasterizer::Colorant(0, 0, 0, 255), bounds, false, name, scene);
+                    bounds.lx = glyphs.ux + font.space * size / float(font.unitsPerEm);
                     int type = sqlite3_column_type(pStmt, i);
                 }
                 do {
+                    bounds.lx = frame.lx, bounds.uy -= (font.ascent - font.descent + font.lineGap) * size / float(font.unitsPerEm);
                     status = sqlite3_step(pStmt);
                     if (status == SQLITE_ROW) {
-                        for (int i = 0; i < columns; i++) {}
+                        for (int i = 0; i < columns; i++) {
+                            glyphs = RasterizerTrueType::writeGlyphs(font, size, Rasterizer::Colorant(0, 0, 0, 255), bounds, false, "{}", scene);
+                            bounds.lx = glyphs.ux + font.space * size / float(font.unitsPerEm);
+                        }
                     }
                 } while (status == SQLITE_ROW);
             }
