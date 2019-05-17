58b4c0cf2ec80c9f7593d6b9222a70a7441ce9a0
Nigel Barber <nigel@mindbrix.co.uk>
2019-05-17 14:42:51 +0100

    RasterizerSQL::DB::writeQuery() - compact, simplify row loop

--- Rasterizer/Rasterizer/RasterizerSQL.hpp
+++ Rasterizer/Rasterizer/RasterizerSQL.hpp
@@ -37,8 +37,7 @@ struct RasterizerSQL {
             char sql[strlen(select) + strlen(table) + 1];
             sql[0] = 0, strcat(sql, select), strcat(sql, table);
             sqlite3_stmt *pStmt;
-            int status = sqlite3_prepare_v2(db, sql, -1, & pStmt, NULL);
-            if (status == SQLITE_OK) {
+            if (sqlite3_prepare_v2(db, sql, -1, & pStmt, NULL) == SQLITE_OK) {
                 Rasterizer::Colorant red(0, 0, 255, 255), black(0, 0, 0, 255);
                 Rasterizer::Bounds glyphs, bounds = frame;
                 float s = size / float(font.unitsPerEm), space = font.space * s * columnSpaces, lineHeight = (font.ascent - font.descent + font.lineGap) * s;
@@ -47,17 +46,13 @@ struct RasterizerSQL {
                     bounds.lx = frame.lx + i * space, bounds.ux = bounds.lx + space;
                     glyphs = RasterizerTrueType::writeGlyphs(font, size, red, bounds, false, true, sqlite3_column_name(pStmt, i), scene);
                 }
-                do {
+                for (int status = sqlite3_step(pStmt); status == SQLITE_ROW; status = sqlite3_step(pStmt)) {
                     bounds.uy -= lineHeight;
-                    status = sqlite3_step(pStmt);
-                    if (status == SQLITE_ROW) {
-                        for (int i = 0; i < columns; i++) {
-                            int type = sqlite3_column_type(pStmt, i);
-                            bounds.lx = frame.lx + i * space, bounds.ux = bounds.lx + space;
-                            glyphs = RasterizerTrueType::writeGlyphs(font, size, black, bounds, false, true, (const char *)sqlite3_column_text(pStmt, i), scene);
-                        }
+                    for (int i = 0; i < columns; i++) {
+                        bounds.lx = frame.lx + i * space, bounds.ux = bounds.lx + space;
+                        glyphs = RasterizerTrueType::writeGlyphs(font, size, black, bounds, false, true, (const char *)sqlite3_column_text(pStmt, i), scene);
                     }
-                } while (status == SQLITE_ROW);
+                }
                 return Rasterizer::Bounds(frame.lx, bounds.uy - lineHeight, bounds.ux, frame.uy);
             }
             sqlite3_finalize(pStmt);
