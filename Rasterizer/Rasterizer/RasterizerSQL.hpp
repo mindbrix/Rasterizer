b2711cfdceaf58ba66818ce7077581b9970b88ac
Nigel Barber <nigel@mindbrix.co.uk>
2019-05-19 14:57:22 +0100

    RasterizerSQL::DB::writeTable() - write header to separate scene

--- Rasterizer/Rasterizer/RasterizerSQL.hpp
+++ Rasterizer/Rasterizer/RasterizerSQL.hpp
@@ -32,20 +32,21 @@ struct RasterizerSQL {
             return count;
         }
         Rasterizer::Bounds writeTable(RasterizerTrueType::Font& font, float size, int columnSpaces, Rasterizer::Bounds frame, const char *table, Rasterizer::SceneList& list) {
-            Rasterizer::Scene& scene = list.addScene();
             int count = rowCount(table);
             const char *select = "SELECT * FROM ";
             char sql[strlen(select) + strlen(table) + 1];
             sql[0] = 0, strcat(sql, select), strcat(sql, table);
             sqlite3_stmt *pStmt;
             if (sqlite3_prepare_v2(db, sql, -1, & pStmt, NULL) == SQLITE_OK) {
+                Rasterizer::Scene& header = list.addScene();
                 Rasterizer::Colorant red(0, 0, 255, 255), black(0, 0, 0, 255);
                 float s = size / float(font.unitsPerEm), columnWidth = font.space * s * columnSpaces, lineHeight = (font.ascent - font.descent + font.lineGap) * s;
                 int columns = sqlite3_column_count(pStmt), rows = 1;
                 for (int i = 0; i < columns; i++) {
                     Rasterizer::Bounds bounds = { frame.lx + i * columnWidth, frame.ly, frame.lx + (i + 1) * columnWidth, frame.uy };
-                    RasterizerTrueType::writeGlyphs(font, size, red, bounds, false, true, sqlite3_column_name(pStmt, i), scene);
+                    RasterizerTrueType::writeGlyphs(font, size, red, bounds, false, true, sqlite3_column_name(pStmt, i), header);
                 }
+                Rasterizer::Scene& scene = list.addScene();
                 for (int status = sqlite3_step(pStmt); status == SQLITE_ROW; status = sqlite3_step(pStmt), rows++)
                     for (int i = 0; i < columns; i++) {
                         Rasterizer::Bounds bounds = { frame.lx + i * columnWidth, frame.ly, frame.lx + (i + 1) * columnWidth, frame.uy - rows * lineHeight };
