89ae9ba048a4454306c2d2bcc072a16860306d04
Nigel Barber <nigel@mindbrix.co.uk>
2019-05-20 17:32:05 +0100

    RasterizerSQL::DB::writeTable() - simplify return bounds

--- Rasterizer/Rasterizer/RasterizerSQL.hpp
+++ Rasterizer/Rasterizer/RasterizerSQL.hpp
@@ -30,16 +30,15 @@ struct RasterizerSQL {
             return count;
         }
         Rasterizer::Bounds writeTable(RasterizerTrueType::Font& font, float size, int columnSpaces, int rowSize, float t, Rasterizer::Bounds frame, const char *table, Rasterizer::SceneList& list) {
-            Rasterizer::Bounds bnds(0.f, 0.f, 0.f, 0.f);
-            int count = rowCount(table);
+            int count = rowCount(table), columns = 0, rows = 0;
+            float s = size / float(font.unitsPerEm), columnWidth = font.space * s * columnSpaces, lineHeight = (font.ascent - font.descent + font.lineGap) * s;
+            Rasterizer::Colorant red(0, 0, 255, 255), black(0, 0, 0, 255);
             char *sql;
             asprintf(& sql, "SELECT * FROM %s LIMIT %d", table, rowSize);
             sqlite3_stmt *pStmt;
             if (sqlite3_prepare_v2(db, sql, -1, & pStmt, NULL) == SQLITE_OK) {
                 Rasterizer::Scene& header = list.addScene();
-                Rasterizer::Colorant red(0, 0, 255, 255), black(0, 0, 0, 255);
-                float s = size / float(font.unitsPerEm), columnWidth = font.space * s * columnSpaces, lineHeight = (font.ascent - font.descent + font.lineGap) * s;
-                int columns = sqlite3_column_count(pStmt), rows = 1;
+                columns = sqlite3_column_count(pStmt), rows = 1;
                 for (int i = 0; i < columns; i++) {
                     Rasterizer::Bounds bounds = { frame.lx + i * columnWidth, frame.ly, frame.lx + (i + 1) * columnWidth, frame.uy };
                     RasterizerTrueType::writeGlyphs(font, size, red, bounds, false, true, sqlite3_column_name(pStmt, i), header);
@@ -51,11 +50,10 @@ struct RasterizerSQL {
                         RasterizerTrueType::writeGlyphs(font, size, black, bounds, false, true, (const char *)sqlite3_column_text(pStmt, i), scene);
                     }
                 }
-                bnds = { frame.lx, frame.uy - rows * lineHeight, frame.lx + columns * columnWidth, frame.uy };
             }
             sqlite3_finalize(pStmt);
             free(sql);
-            return bnds;
+            return { frame.lx, frame.uy - rows * lineHeight, frame.lx + columns * columnWidth, frame.uy };
         }
         sqlite3 *db = nullptr;
     };
