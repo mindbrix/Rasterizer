54a1a205ee349366a129d5e6810dcbf67f2ee9f1
Nigel Barber <nigel@mindbrix.co.uk>
2019-05-24 09:38:54 +0100

    kCreateViewsTable - populate Views

--- Rasterizer/Rasterizer/RasterizerSQL.hpp
+++ Rasterizer/Rasterizer/RasterizerSQL.hpp
@@ -13,7 +13,7 @@
 struct RasterizerSQL {
     struct DB {
         static constexpr const char *kCreateFontsTable = "DROP TABLE Fonts; CREATE TABLE Fonts(name text, url text, display text, family text, style text);";
-        static constexpr const char *kCreateViewsTable = "CREATE TABLE Views(Fonts_name varchar(64), table varchar(32), order smallint);";
+        static constexpr const char *kCreateViewsTable = "CREATE TABLE Views(Fonts_name varchar(64), tbl varchar(32), idx smallint); INSERT INTO Views (Fonts_name, tbl, idx) SELECT 'Helvetica', name, 0 FROM sqlite_master;";
         
         ~DB() { close(); }
         int open(const char *filename) {
