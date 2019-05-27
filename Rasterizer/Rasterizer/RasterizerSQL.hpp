e1bdf77f7c99551a95a755f478a0bd0e5d38147d
Nigel Barber <nigel@mindbrix.co.uk>
2019-05-27 06:47:56 +0100

    RasterizerSQL::DB - insert select WIP

--- Rasterizer/Rasterizer/RasterizerSQL.hpp
+++ Rasterizer/Rasterizer/RasterizerSQL.hpp
@@ -13,7 +13,12 @@
 struct RasterizerSQL {
     struct DB {
         static constexpr const char *kFontsTable = "fonts", *kFontFamilyTable = "fontFamily", *kFontStyleTable = "fontStyle";
-        static constexpr const char *kCreateFontsTable = "CREATE TABLE IF NOT EXISTS fonts(family varchar(64), style varchar(32), name varchar(64), url text); CREATE TABLE IF NOT EXISTS fontFamily(family varchar(64) UNIQUE); CREATE TABLE IF NOT EXISTS fontStyle(style varchar(32) UNIQUE);";
+        static constexpr const char *kInsertSelect = "SELECT 'test', fontFamily.rowid, fontStyle.rowid FROM fontFamily, fontStyle WHERE family = 'Helvetica' AND style = 'Bold';";
+        static constexpr const char *kCreateFontsTable = "\
+        CREATE TABLE IF NOT EXISTS _fonts(name varchar(64), url text, fontFamily_rowid int, fontStyle_rowid int); \
+        CREATE TABLE IF NOT EXISTS fonts(family varchar(64), style varchar(32), name varchar(64), url text); \
+        CREATE TABLE IF NOT EXISTS fontFamily(family varchar(64) UNIQUE); \
+        CREATE TABLE IF NOT EXISTS fontStyle(style varchar(32) UNIQUE);";
         static constexpr const char *kSelectTables = "SELECT tbl_name FROM sqlite_master ORDER BY tbl_name ASC;";
         const int kColumnSpaces = 6, kColumnCount = 5, kRowSize = 8;
         
