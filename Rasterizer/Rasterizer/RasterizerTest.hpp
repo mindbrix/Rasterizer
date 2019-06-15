1a45d8298524ce8dba4d10e0485d6bc9a71c6833
Nigel Barber <nigel@mindbrix.co.uk>
2019-06-15 22:27:13 +0100

    RasterizerTest::addTestPaths - simplify, compact

--- Rasterizer/Rasterizer/RasterizerTest.hpp
+++ Rasterizer/Rasterizer/RasterizerTest.hpp
@@ -10,17 +10,12 @@
 struct RasterizerTest {
     static void addTestPaths(Rasterizer::Scene& scene) {
         Rasterizer::Colorant color(0, 0, 0, 255);
-        if (0) {
-            Rasterizer::Path shape;
-            shape.ref->addBounds(Rasterizer::Bounds(100.5, 100.5, 199.5, 199.5));
-            scene.addPath(shape, Rasterizer::Transform::identity(), color);
-        }
-        if (0) {
+        if (0)
+            scene.addBounds(Rasterizer::Bounds(100.5, 100.5, 199.5, 199.5), Rasterizer::Transform::identity(), color);
+        if (0)
             scene.addPath(createPhyllotaxisPath(100000), Rasterizer::Transform::identity(), color);
-        }
-        if (0) {
+        if (0)
             writePhyllotaxisToScene(100000, scene);
-        }
     }
     static void writePhyllotaxisToScene(size_t count, Rasterizer::Scene& scene) {
         Rasterizer::Colorant color(0, 0, 0, 255);
