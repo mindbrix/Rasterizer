1c2907e35d98239d7675dc20935cdc79d27e7c1a
Nigel Barber <nigel@mindbrix.co.uk>
2019-06-23 01:10:37 +0100

    Rasterizer::writeShape() - compact, shape --> ctm, simplify

--- Rasterizer/Rasterizer/Rasterizer.hpp
+++ Rasterizer/Rasterizer/Rasterizer.hpp
@@ -1042,16 +1042,14 @@ struct Rasterizer {
                 }
         }
     }
-    static void writeShape(Bounds clip, Transform shape, bool circle, uint8_t *src, Bitmap *bitmap) {
+    static void writeShape(Bounds clip, Transform ctm, bool circle, uint8_t *src, Bitmap *bitmap) {
         float rl, bx, by, d[4], dx[2], dy[2], r;
         float src0 = src[0], src1 = src[1], src2 = src[2], srcAlpha = src[3] * 0.003921568627f, y, x, del, d0, d1, d2, d3, cx, cy, alpha;
-        bx = clip.lx - shape.tx, by = clip.ly - shape.ty;
-        rl = 1.f / sqrtf(shape.c * shape.c + shape.d * shape.d);
-        d[0] = rl * (shape.c * by - shape.d * bx), d[1] = rl * (shape.c * (by - shape.b) - shape.d * (bx - shape.a));
-        dx[0] = rl * -shape.d, dy[0] = rl * shape.c;
-        rl = 1.f / sqrtf(shape.a * shape.a + shape.b * shape.b);
-        d[2] = rl * (shape.a * by - shape.b * bx), d[3] = rl * (shape.a * (by - shape.d) - shape.b * (bx - shape.c));
-        dx[1] = rl * -shape.b, dy[1] = rl * shape.a;
+        bx = clip.lx - ctm.tx, by = clip.ly - ctm.ty;
+        rl = 1.f / sqrtf(ctm.c * ctm.c + ctm.d * ctm.d), dx[0] = rl * -ctm.d, dy[0] = rl * ctm.c;
+        d[0] = rl * (ctm.c * by - ctm.d * bx), d[1] = d[0] + rl * (ctm.a * ctm.d - ctm.b * ctm.c);
+        rl = 1.f / sqrtf(ctm.a * ctm.a + ctm.b * ctm.b), dx[1] = rl * -ctm.b, dy[1] = rl * ctm.a;
+        d[2] = rl * (ctm.a * by - ctm.b * bx), d[3] = d[2] - rl * (ctm.a * ctm.d - ctm.b * ctm.c);
         r = fmaxf(1.f, fminf(1.f + d[1] - d[0], 1.f + d[2] - d[3]) * 0.5f);
         uint8_t *pixelAddress = pixelAddress = bitmap->pixelAddress(clip.lx, clip.ly), *pixel;
         for (y = clip.ly; y < clip.uy; y++, pixelAddress -= bitmap->stride) {
