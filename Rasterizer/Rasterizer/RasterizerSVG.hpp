fad535aa04ae9ab13d5d8f84ccc0c6787ad5312e
Nigel Barber <nigel@mindbrix.co.uk>
2019-06-23 23:07:42 +0100

    RasterizerSVG::writeScene() - test stroke paths with isDrawable()

--- Rasterizer/Rasterizer/RasterizerSVG.hpp
+++ Rasterizer/Rasterizer/RasterizerSVG.hpp
@@ -39,18 +39,20 @@ struct RasterizerSVG {
                     scene.addPath(createPathFromShape(shape), flip, colorFromPaint(shape->fill));
                 if (shape->stroke.type == NSVG_PAINT_COLOR && shape->strokeWidth) {
                     Rasterizer::Path s = createPathFromShape(shape);
-                    float w = s.ref->bounds.ux - s.ref->bounds.lx, h = s.ref->bounds.uy - s.ref->bounds.ly, dim = w < h ? w : h;
-                    CGMutablePathRef path = CGPathCreateMutable();
-                    RasterizerCG::writePathToCGPath(shape->fill.type == NSVG_PAINT_NONE ? s : scene.paths.back(), path);
-                    CGPathRef stroked = RasterizerCG::createStrokedPath(path, shape->strokeWidth,
-                        shape->strokeLineCap == NSVG_CAP_BUTT ? kCGLineCapButt : shape->strokeLineCap == NSVG_CAP_SQUARE ? kCGLineCapSquare : kCGLineCapRound,
-                        shape->strokeLineJoin == NSVG_JOIN_MITER ? kCGLineJoinMiter : shape->strokeLineJoin == NSVG_JOIN_ROUND ? kCGLineJoinRound : kCGLineJoinBevel,
-                        shape->miterLimit, shape->strokeWidth > dim ? 1 : 10);
-                    Rasterizer::Path p = RasterizerCG::createPathFromCGPath(stroked);
-                    if (p.ref->atomsCount < 32767)
-                        scene.addPath(p, flip, colorFromPaint(shape->stroke));
-                    CGPathRelease(path);
-                    CGPathRelease(stroked);
+                    if (s.ref->isDrawable()) {
+                        float w = s.ref->bounds.ux - s.ref->bounds.lx, h = s.ref->bounds.uy - s.ref->bounds.ly, dim = w < h ? w : h;
+                        CGMutablePathRef path = CGPathCreateMutable();
+                        RasterizerCG::writePathToCGPath(shape->fill.type == NSVG_PAINT_NONE ? s : scene.paths.back(), path);
+                        CGPathRef stroked = RasterizerCG::createStrokedPath(path, shape->strokeWidth,
+                                                                            shape->strokeLineCap == NSVG_CAP_BUTT ? kCGLineCapButt : shape->strokeLineCap == NSVG_CAP_SQUARE ? kCGLineCapSquare : kCGLineCapRound,
+                                                                            shape->strokeLineJoin == NSVG_JOIN_MITER ? kCGLineJoinMiter : shape->strokeLineJoin == NSVG_JOIN_ROUND ? kCGLineJoinRound : kCGLineJoinBevel,
+                                                                            shape->miterLimit, shape->strokeWidth > dim ? 1 : 10);
+                        Rasterizer::Path p = RasterizerCG::createPathFromCGPath(stroked);
+                        if (p.ref->atomsCount < 32767)
+                            scene.addPath(p, flip, colorFromPaint(shape->stroke));
+                        CGPathRelease(path);
+                        CGPathRelease(stroked);
+                    }
                 }
             }
             nsvgDelete(image);
