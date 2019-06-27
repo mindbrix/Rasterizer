ee2b014c28be5ee6bc92f3c8560bd8b0ab4086e2
Nigel Barber <nigel@mindbrix.co.uk>
2019-06-27 22:23:47 +0100

    GPU - add frameCount

--- Rasterizer/Rasterizer/Rasterizer.hpp
+++ Rasterizer/Rasterizer/Rasterizer.hpp
@@ -630,7 +630,7 @@ struct Rasterizer {
         void reset() {
             shapesCount = shapePaths = outlinePaths = 0, indices.reset(), blends.reset(), opaques.reset(), outlines.reset(), ctms = nullptr, molecules.reset(), cache.reset(), sceneCache.reset();
         }
-        size_t shapesCount = 0, shapePaths = 0, outlinePaths = 0;
+        size_t shapesCount = 0, shapePaths = 0, outlinePaths = 0, frameCount = 0;
         Allocator allocator;
         Molecules molecules;
         Row<Index> indices;
@@ -720,7 +720,7 @@ struct Rasterizer {
             size_t size = ceilf(float(height) * krfh);
             if (segments.size() != size)
                 segments.resize(size);
-            gpu.allocator.init(width, height), gpu.ctms = ctms;
+            gpu.allocator.init(width, height), gpu.ctms = ctms, gpu.frameCount++;
         }
         void drawScenes(SceneList& list, Transform *ctms, bool even, Colorant *colors, Transform *clips, float width, size_t slz, size_t suz) {
             size_t lz, uz, i, clz, cuz;
