cea003212464f0d43eead8beb2941102fe44f2d3
Nigel Barber <nigel@mindbrix.co.uk>
2019-07-01 19:07:30 +0100

    RasterizerSVG - Rasterizer:: --> Ra::

--- Rasterizer/Rasterizer/RasterizerSVG.hpp
+++ Rasterizer/Rasterizer/RasterizerSVG.hpp
@@ -9,14 +9,14 @@
 #import "nanosvg.h"
 
 struct RasterizerSVG {
-    static Rasterizer::Colorant colorFromPaint(NSVGpaint paint) {
+    static Ra::Colorant colorFromPaint(NSVGpaint paint) {
         if (paint.type == NSVG_PAINT_COLOR)
-            return Rasterizer::Colorant((paint.color >> 16) & 0xFF, (paint.color >> 8) & 0xFF, paint.color & 0xFF, paint.color >> 24);
+            return Ra::Colorant((paint.color >> 16) & 0xFF, (paint.color >> 8) & 0xFF, paint.color & 0xFF, paint.color >> 24);
         else
-            return Rasterizer::Colorant(0, 0, 0, 64);
+            return Ra::Colorant(0, 0, 0, 64);
     }
-    static Rasterizer::Path createPathFromShape(NSVGshape *shape) {
-        Rasterizer::Path p;
+    static Ra::Path createPathFromShape(NSVGshape *shape) {
+        Ra::Path p;
         float *pts;
         int i;
         for (NSVGpath *path = shape->paths; path != NULL; path = path->next) {
@@ -27,18 +27,18 @@ struct RasterizerSVG {
         }
         return p;
     }
-    static void writeScene(const void *bytes, size_t size, Rasterizer::Scene& scene) {
+    static void writeScene(const void *bytes, size_t size, Ra::Scene& scene) {
         char *data = (char *)malloc(size + 1);
         memcpy(data, bytes, size);
         data[size] = 0;
         struct NSVGimage* image = data ? nsvgParse(data, "px", 96) : NULL;
         if (image) {
-            Rasterizer::Transform flip(1, 0, 0, -1, 0, image->height);
+            Ra::Transform flip(1, 0, 0, -1, 0, image->height);
             for (NSVGshape *shape = image->shapes; shape != NULL; shape = shape->next) {
                 if (shape->fill.type == NSVG_PAINT_COLOR)
                     scene.addPath(createPathFromShape(shape), flip, colorFromPaint(shape->fill));
                 if (shape->stroke.type == NSVG_PAINT_COLOR && shape->strokeWidth) {
-                    Rasterizer::Path s = createPathFromShape(shape);
+                    Ra::Path s = createPathFromShape(shape);
                     if (s.ref->isDrawable) {
                         float w = s.ref->bounds.ux - s.ref->bounds.lx, h = s.ref->bounds.uy - s.ref->bounds.ly, dim = w < h ? w : h;
                         CGMutablePathRef path = CGPathCreateMutable();
@@ -47,7 +47,7 @@ struct RasterizerSVG {
                                                                             shape->strokeLineCap == NSVG_CAP_BUTT ? kCGLineCapButt : shape->strokeLineCap == NSVG_CAP_SQUARE ? kCGLineCapSquare : kCGLineCapRound,
                                                                             shape->strokeLineJoin == NSVG_JOIN_MITER ? kCGLineJoinMiter : shape->strokeLineJoin == NSVG_JOIN_ROUND ? kCGLineJoinRound : kCGLineJoinBevel,
                                                                             shape->miterLimit, shape->strokeWidth > dim ? 1 : 10);
-                        Rasterizer::Path p = RasterizerCG::createPathFromCGPath(stroked);
+                        Ra::Path p = RasterizerCG::createPathFromCGPath(stroked);
                         if (p.ref->atomsCount < 32767)
                             scene.addPath(p, flip, colorFromPaint(shape->stroke));
                         CGPathRelease(path);
