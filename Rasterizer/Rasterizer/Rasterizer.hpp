e2c7052ea2405bb91cfd4b6390336eddd31d3faf
Nigel Barber <nigel@mindbrix.co.uk>
2019-07-06 16:39:39 +0100

    writeGPUPath() - simplify

--- Rasterizer/Rasterizer/Rasterizer.hpp
+++ Rasterizer/Rasterizer/Rasterizer.hpp
@@ -628,11 +628,10 @@ struct Rasterizer {
                 else if (slow)
                     gpu.cache.writeCachedOutline(entry, m, clip, segments);
                 if (entry && !slow) {
-                    size_t count = entry->seg.end - entry->seg.begin, eidx = entry - gpu.cache.entries.base;
                     gpu.ctms[iz] = m;
                     GPU::Instance *inst = new (gpu.blends.alloc(1)) GPU::Instance(iz, GPU::Instance::kMolecule);
                     inst->quad.cell = gpu.allocator.alloc(clip.lx, clip.ly, clip.ux, clip.uy, gpu.blends.end, path.ref->molecules.size(), entry->instances, true),
-                    inst->quad.cover = 0, inst->quad.count = uint16_t(count), inst->quad.iy = int(eidx), inst->quad.begin = 0, inst->quad.base = int(entry->seg.begin);
+                    inst->quad.cover = 0, inst->quad.count = uint16_t(entry->seg.end - entry->seg.begin), inst->quad.iy = int(entry - gpu.cache.entries.base), inst->quad.begin = 0, inst->quad.base = int(entry->seg.begin);
                 } else
                     writeSegments(segments.segments, clip, even, iz, src[3] == 255 && !hit, gpu);
             }
