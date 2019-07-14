5acb8bc0db4192a08b78b1494db4e4584549eece
Nigel Barber <nigel@mindbrix.co.uk>
2019-07-14 18:13:04 +0100

    Geometry - add sqrsums[]

--- Rasterizer/Rasterizer/Rasterizer.hpp
+++ Rasterizer/Rasterizer/Rasterizer.hpp
@@ -90,7 +90,7 @@ struct Rasterizer {
     };
     struct Geometry {
         enum Type { kNull = 0, kMove, kLine, kQuadratic, kCubic, kClose, kCountSize };
-        Geometry() : atomsCount(0), shapesCount(0), px(0), py(0), shapes(nullptr), circles(nullptr), isGlyph(false), isDrawable(false), isPolygon(true), refCount(0), hash(0) { bzero(counts, sizeof(counts)); }
+        Geometry() : atomsCount(0), shapesCount(0), px(0), py(0), shapes(nullptr), circles(nullptr), isGlyph(false), isDrawable(false), isPolygon(true), refCount(0), hash(0) { bzero(counts, sizeof(counts)), bzero(sqrsums, sizeof(sqrsums)); }
         ~Geometry() { if (shapes) free(shapes), free(circles); }
         
         float *alloc(Type type, size_t size) {
@@ -105,6 +105,15 @@ struct Rasterizer {
             counts[type]++, hash = ::crc64(::crc64(hash, & type, sizeof(type)), p, size * 2 * sizeof(float));
             if (type == kMove)
                 molecules.emplace_back(Bounds()), mols = & molecules[0];
+            else if (type == kQuadratic) {
+                float *q = p - 2, ax = q[0] + q[4] - q[2] - q[2], ay = q[1] + q[5] - q[3] - q[3];
+                sqrsums[type] += sqrtf(sqrtf(ax * ax + ay * ay));
+            } else if (type == kCubic) {
+                float *c = p - 2, cx, bx, ax, cy, by, ay;
+                cx = 3.f * (c[2] - c[0]), bx = 3.f * (c[4] - c[2]) - cx, ax = c[6] - c[0] - cx - bx;
+                cy = 3.f * (c[3] - c[1]), by = 3.f * (c[5] - c[3]) - cy, ay = c[7] - c[1] - cy - by;
+                sqrsums[type] += sqrtf(sqrtf(ax * ax + ay * ay + bx * bx + by * by));
+            }
             isPolygon &= type != kQuadratic && type != kCubic;
             isDrawable |= !((atomsCount < 3 && shapesCount == 0) || bounds.lx == FLT_MAX);
             weight = atomsCount ?: (shapes ? shapesCount >> 4: 0);
@@ -172,7 +181,7 @@ struct Rasterizer {
         std::vector<uint8_t> types;
         std::vector<float> points;
         std::vector<Bounds> molecules;
-        float px, py, *pts;
+        float px, py, *pts, sqrsums[kCountSize];
         Transform *shapes;
         bool *circles, isGlyph, isDrawable, isPolygon;
         Bounds bounds, *mols;
