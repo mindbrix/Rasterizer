a13b3ffeadb522b372b2acc268d6f681c9bdc0ac
Nigel Barber <nigel@mindbrix.co.uk>
2019-07-31 21:25:28 +0100

    writeShapeDistances() - w[] WIP

--- Rasterizer/Rasterizer/Rasterizer.hpp
+++ Rasterizer/Rasterizer/Rasterizer.hpp
@@ -1005,9 +1005,9 @@ struct Rasterizer {
         bool single = clip.ux - clip.lx < 256.f;
         uint32_t range = single ? powf(2.f, ceilf(log2f(clip.ux - clip.lx + 1.f))) : 256;
         float src0 = src[0], src1 = src[1], src2 = src[2], srcAlpha = src[3] * 0.003921568627f, ly, uy, scale, cover, lx, ux, x, y, *delta, soft = 1.f;
-        float d[4], dx[2], dy[2], r, d0, d1, d2, d3;
+        float d[4], w[2], dx[2], dy[2], r, d0, d1, d2, d3;
         if (hit)
-            writeShapeDistances(clip, clipctm, d, dx, dy, & r);
+            writeShapeDistances(clip, clipctm, d, w, dx, dy, & r);
         Row<Segment> *segments = sgmnts->segments;  Segment *segment;
         Row<Index> indices;  Index *index;
         for (iy = ily; iy < iuy; iy++, segments++) {
@@ -1055,15 +1055,15 @@ struct Rasterizer {
             }
         }
     }
-    static inline void writeShapeDistances(Bounds clip, Transform ctm, float d[4], float dx[2], float dy[2], float *r) {
-        float det, rl0, rl1, del0, del1, w0, w1;
+    static inline void writeShapeDistances(Bounds clip, Transform ctm, float d[4], float w[2], float dx[2], float dy[2], float *r) {
+        float det, rl0, rl1, del0, del1;
         det = ctm.det(), rl0 = copysign(1.f / sqrtf(ctm.c * ctm.c + ctm.d * ctm.d), det), rl1 = copysign(1.f / sqrtf(ctm.a * ctm.a + ctm.b * ctm.b), det);
         dx[0] = rl0 * -ctm.d, dy[0] = rl0 * ctm.c, dx[1] = rl1 * -ctm.b, dy[1] = rl1 * ctm.a;
         del0 = rl0 * (ctm.c * (clip.ly - ctm.ty) - ctm.d * (clip.lx - ctm.tx)) + 0.5f * (dx[0] + dy[0]),
         del1 = rl1 * (ctm.a * (clip.ly - ctm.ty) - ctm.b * (clip.lx - ctm.tx)) + 0.5f * (dx[1] + dy[1]);
-        w0 = 1.f + rl0 * det, w1 = 1.f + rl1 * det;
-        d[0] = 0.5f - del0, d[1] = w0 - d[0], d[2] = 0.5f + del1, d[3] = w1 - d[2];
-        *r = fmaxf(1.f, fminf(w0, w1) * 0.5f);
+        w[0] = 1.f + rl0 * det, w[1] = 1.f + rl1 * det;
+        d[0] = 0.5f - del0, d[1] = w[0] - d[0], d[2] = 0.5f + del1, d[3] = w[1] - d[2];
+        *r = fmaxf(1.f, fminf(w[0], w[1]) * 0.5f);
     }
     static void writeDeltaPixels(Output *del, Bounds clip, bool hit, Transform clipctm, bool even, uint8_t *src, Bitmap *bitmap) {
         float *deltas = del->deltas;
@@ -1071,9 +1071,9 @@ struct Rasterizer {
             for (float y = clip.ly; y < clip.uy; y++, deltas += del->stride)
                 *deltas = 0.f;
         else {
-            float d[4], dx[2], dy[2], r, dd, d0, d1, d2, d3;
+            float d[4], w[2], dx[2], dy[2], r, d0, d1, d2, d3;
             if (hit)
-                writeShapeDistances(clip, clipctm, d, dx, dy, & r);
+                writeShapeDistances(clip, clipctm, d, w, dx, dy, & r);
             uint8_t *rowaddr = bitmap->pixelAddress(clip.lx, clip.ly), *pixel;
             float src0 = src[0], src1 = src[1], src2 = src[2], srcAlpha = src[3] * 0.003921568627f, y, x, cover, alpha, *delta, soft = 1.f;
             for (y = 0.f; y < clip.uy - clip.ly; y++, deltas += del->stride, rowaddr -= bitmap->stride, *delta = 0.f)
@@ -1081,7 +1081,7 @@ struct Rasterizer {
                     if (*delta)
                         cover += *delta, *delta = 0.f, alpha = alphaForCover(cover, even);
                     if (hit) {
-                        dd = x * dx[0] + y * dy[0], d0 = d[0] - dd, d1 = d[1] + dd, dd = x * dx[1] + y * dy[1], d2 = d[2] + dd, d3 = d[3] - dd;
+                        d0 = d[0] - (x * dx[0] + y * dy[0]), d1 = w[0] - d0, d2 = d[2] + (x * dx[1] + y * dy[1]), d3 = w[1] - d2;
                         soft = (d0 < 0.f ? 0.f : d0 > 1.f ? 1.f : d0) * (d1 < 0.f ? 0.f : d1 > 1.f ? 1.f : d1) * (d2 < 0.f ? 0.f : d2 > 1.f ? 1.f : d2) * (d3 < 0.f ? 0.f : d3 > 1.f ? 1.f : d3);
                     }
                     if (alpha * soft > 0.003921568627f)
@@ -1091,8 +1091,8 @@ struct Rasterizer {
     }
     static void writeShapePixels(Bounds clip, Transform clipctm, Transform ctm, bool circle, uint8_t *src, float f, Bitmap *bitmap) {
         float src0 = src[0], src1 = src[1], src2 = src[2], srcAlpha = src[3] * 0.003921568627f;
-        float d[4], dx[2], dy[2], r, y, x, dd, d0, d1, d2, d3, cx, cy, m0, m1, alpha;
-        writeShapeDistances(clip, ctm, d, dx, dy, & r);
+        float d[4], w[2], dx[2], dy[2], r, y, x, dd, d0, d1, d2, d3, cx, cy, m0, m1, alpha;
+        writeShapeDistances(clip, ctm, d, w, dx, dy, & r);
         uint8_t *rowaddr = bitmap->pixelAddress(clip.lx, clip.ly), *pixel = rowaddr;
         for (y = clip.ly; y < clip.uy; y++, rowaddr -= bitmap->stride, pixel = rowaddr) {
             dd = (y - clip.ly) * dy[0], d0 = d[0] - dd, d1 = d[1] + dd, dd = (y - clip.ly) * dy[1], d2 = d[2] + dd, d3 = d[3] - dd;
