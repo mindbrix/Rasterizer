5d2dcf99b3004d4165c5a5eec8cd0efc324e3814
Nigel Barber <nigel@mindbrix.co.uk>
2019-11-09 12:04:40 +0000

    RasterizerState - default useCurves to true

--- Rasterizer/Rasterizer/RasterizerState.hpp
+++ Rasterizer/Rasterizer/RasterizerState.hpp
@@ -113,7 +113,7 @@ struct RasterizerState {
         events.resize(0);
         return redraw;
     }
-    bool keyDown = false, mouseDown = false, mouseMove = false, useCurves = false;
+    bool keyDown = false, mouseDown = false, mouseMove = false, useCurves = true;
     float x, y, scale, outlineWidth = 0.f;
     int keyCode = 0;
     size_t index = INT_MAX, flags = 0;
