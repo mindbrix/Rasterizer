a709259b4329cbc02c0801c0552016a958dcd530
Nigel Barber <nigel@mindbrix.co.uk>
2019-11-13 08:45:49 +0000

    divideQuadratic() - simplify t

--- Rasterizer/Rasterizer/Rasterizer.hpp
+++ Rasterizer/Rasterizer/Rasterizer.hpp
@@ -800,7 +800,7 @@ struct Rasterizer {
             (*function)(mx, my, x2, y2, 2, info);
         } else {
             float tx0, ty0, tx1, ty1, x, y;
-            t = (eccentric || !monotone) ? 0.5f : t < 0.5f ? t * 2.f : 1.0f - 2.f * (1.f - t), s = 1.f - t;
+            t = eccentric || !monotone ? 0.5f : t < 0.5f ? t * 2.f : t * 2.f - 1.f, s = 1.f - t;
             tx0 = s * x0 + t * x1, tx1 = s * x1 + t * x2, x = tx0 * s + tx1 * t;
             ty0 = s * y0 + t * y1, ty1 = s * y1 + t * y2, y = ty0 * s + ty1 * t;
             divideQuadratic(x0, y0, tx0, ty0, x, y, function, info);
