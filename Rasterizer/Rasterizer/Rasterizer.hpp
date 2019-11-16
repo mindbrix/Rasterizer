2989bd76f5c2d93d3ea58e62e0cf618c8a3f8af1
Nigel Barber <nigel@mindbrix.co.uk>
2019-11-16 08:51:46 +0000

    SceneWriter::createBuffer() - allocate, write & populate

--- Rasterizer/Rasterizer/Rasterizer.hpp
+++ Rasterizer/Rasterizer/Rasterizer.hpp
@@ -227,33 +227,60 @@ struct Rasterizer {
     };
     
     struct SceneBuffer {
+        static constexpr size_t kPageSize = 4096;
         size_t idx0(size_t is) { return pidxs[is] == 0 ? 0 : ends[pidxs[is] - 1]; }
         size_t idx1(size_t is) { return ends[pidxs[is]]; }
 
         uint8_t *base = nullptr;
-        Segment *segments; int16_t *offsets; uint32_t *midxs, *ends, *pidxs; Bounds *AABBs, *molecules;
+        size_t size = 0, count = 0;
+        Segment *segments; int16_t *offsets; uint32_t *midxs, *ends, *pidxs; Bounds *bounds, *molecules;
     };
     
     struct SceneWriter {
         SceneBuffer createBuffer(Scene& scene) {
-            SceneBuffer buffer;
             for (int i = 0; i < scene.count; i++) {
                 Path path = scene.paths[i];
                 auto it = cache.find(path->hash);
                 if (it != cache.end())
                     pidxs.emplace_back(it->second);
                 else {
-                    cache.emplace(path->hash, AABBs.size());
-                    pidxs.emplace_back(AABBs.size());
+                    cache.emplace(path->hash, bounds.size());
+                    pidxs.emplace_back(bounds.size());
                     
                     midx = molecules.size();
                     writePath(path.ref, Transform(), Bounds(), true, true, true, writeSegment, this);
                     ends.emplace_back(segments.size());
-                    AABBs.emplace_back(path->bounds);
+                    bounds.emplace_back(path->bounds);
                     for (Bounds& m : path->molecules)
                         molecules.emplace_back(m);
                 }
             }
+            size_t ssegments, soffsets, sbounds, smolecules, smidxs, sends, spidxs, size, begin;
+            SceneBuffer buffer;
+            buffer.count = scene.count;
+            ssegments = segments.size() * sizeof(segments[0]), soffsets = offsets.size() * sizeof(offsets[0]);
+            smidxs = midxs.size() * sizeof(midxs[0]), sends = ends.size() * sizeof(ends[0]);
+            sbounds = bounds.size() * sizeof(bounds[0]), smolecules = molecules.size() * sizeof(molecules[0]);
+            spidxs = pidxs.size() * sizeof(pidxs[0]);
+            size = ssegments + soffsets + smidxs + sends + sbounds + smolecules + spidxs;
+            buffer.size = (size + SceneBuffer::kPageSize - 1) / SceneBuffer::kPageSize * SceneBuffer::kPageSize;
+            posix_memalign((void **)& buffer.base, SceneBuffer::kPageSize, buffer.size);
+            begin = 0;
+            buffer.segments = (Segment *)(buffer.base + begin), begin += ssegments;
+            buffer.offsets = (int16_t *)(buffer.base + begin), begin += soffsets;
+            buffer.midxs = (uint32_t *)(buffer.base + begin), begin += smidxs;
+            buffer.ends = (uint32_t *)(buffer.base + begin), begin += sends;
+            buffer.bounds = (Bounds *)(buffer.base + begin), begin += sbounds;
+            buffer.molecules = (Bounds *)(buffer.base + begin), begin += smolecules;
+            buffer.pidxs = (uint32_t *)(buffer.base + begin), begin += spidxs;
+            assert(size == begin);
+            memcpy(buffer.segments, & segments[0], ssegments);
+            memcpy(buffer.offsets, & offsets[0], soffsets);
+            memcpy(buffer.midxs, & midxs[0], smidxs);
+            memcpy(buffer.ends, & ends[0], sends);
+            memcpy(buffer.bounds, & bounds[0], sbounds);
+            memcpy(buffer.molecules, & molecules[0], smolecules);
+            memcpy(buffer.pidxs, & pidxs[0], spidxs);
             return buffer;
         }
         static void writeSegment(float x0, float y0, float x1, float y1, uint32_t curve, void *info) {
@@ -285,7 +312,7 @@ struct Rasterizer {
         std::vector<int16_t> offsets;
         std::vector<uint32_t> midxs;
         std::vector<uint32_t> ends;
-        std::vector<Bounds> AABBs, molecules;
+        std::vector<Bounds> bounds, molecules;
         std::vector<uint32_t> pidxs;
         std::unordered_map<size_t, size_t> cache;
     };
