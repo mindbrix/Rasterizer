e47b77ea61e9436321c00f31fdbed23ada680d73
Nigel Barber <nigel@mindbrix.co.uk>
2019-11-17 22:16:15 +0000

    writeContextToBuffer() - encode outline flags into iz

--- Rasterizer/Rasterizer/Rasterizer.hpp
+++ Rasterizer/Rasterizer/Rasterizer.hpp
@@ -1369,12 +1369,16 @@ struct Rasterizer {
                         *((size_t *)(buffer.base + begin)) = buf->hash;
                         entries.emplace_back(Buffer::kSceneBuffer, begin, begin + sizeof(size_t)), begin = entries.back().end;
                     }
-                    uint32_t *dst = (uint32_t *)(buffer.base + begin), idx0, idx1, idx;
+                    uint32_t *dst = (uint32_t *)(buffer.base + begin), idx0, idx1, idx, inz;
                     for (iz = clz; iz < cuz; iz++)
                         if (flags[iz]) {
+                            inz = uint32_t(iz) | GPU::Instance::kOutlines
+                            | (flags[iz] & Scene::kOutlineRounded ? GPU::Instance::kRounded : 0)
+                            | (flags[iz] & Scene::kOutlineEndCap ? GPU::Instance::kEndCap : 0);
+                            
                             ip = iz - lz, idx0 = buf->idx0(ip), idx1 = buf->idx1(ip);
                             for (idx = idx0; idx < idx1; idx += 4)
-                                *dst++ = idx, *dst++ = uint32_t(iz);
+                                *dst++ = idx, *dst++ = inz;
                         }
                 }
             }
