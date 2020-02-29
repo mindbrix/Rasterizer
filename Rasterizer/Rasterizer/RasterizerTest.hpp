b12c38229460bf391d54c1601ea93aaafbfe4ee7
Nigel Barber <nigel@mindbrix.co.uk>
2020-02-29 15:24:54 +0000

    createConcentrichronScene() - simplify fday & fdate

--- Rasterizer/Rasterizer/RasterizerTest.hpp
+++ Rasterizer/Rasterizer/RasterizerTest.hpp
@@ -77,7 +77,7 @@ struct RasterizerTest {
         int year = lt->tm_year + 1900;
         bool isLeapYear = year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
         float fsec = (lt->tm_sec + tv.tv_usec / 1e6f) / 60.f, fmin = (lt->tm_min + fsec) / 60.f, fhour = (lt->tm_hour + fmin) / 24.f;
-        float fday = lt->tm_wday / 7.f + fhour / 7.f, fdate = (lt->tm_mday - 1) / 31.f + fhour / 31.f;
+        float fday = (lt->tm_wday + fhour) / 7.f, fdate = (lt->tm_mday - 1 + fhour) / 31.f;
         float daysthismonth = monthdays[lt->tm_mon] + (lt->tm_mon == 1 && isLeapYear ? 1.f : 0.f);
         float fmonth = lt->tm_mon / 12.f + (lt->tm_mday - 1) / daysthismonth / 12.f;
         float fyear = (lt->tm_year - 120) / 20.f + (lt->tm_yday / (isLeapYear ? 365.f : 364.f)) / 20.f;
