5f63b856211a394ce9ce7c1b8bb66a503a81d364
Nigel Barber <nigel@mindbrix.co.uk>
2020-03-26 16:26:59 +0000

    map2DTo3D() - restore centre

--- Rasterizer/Rasterizer/RasterizerTest.hpp
+++ Rasterizer/Rasterizer/RasterizerTest.hpp
@@ -65,8 +65,8 @@ struct Transform3D {
         x = ix * ctm.a + iy * ctm.c + ctm.tx;
         y = ix * ctm.b + iy * ctm.d + ctm.ty;
         w = m3 * x + m7 * y + m15;
-        ox = (m0 * x + m4 * y + m12) / w * sx;
-        oy = (m1 * x + m5 * y + m13) / w * sy;
+        ox = (m0 * x + m4 * y + m12) / w * sx + sx;
+        oy = (m1 * x + m5 * y + m13) / w * sy + sy;
     }
     float m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15;
 };
