4372a469a65b2708042d246c74b552c7d0627b77
Nigel Barber <nigel@mindbrix.co.uk>
2020-05-14 12:51:18 +0100

    Transform::det() - mark as const

--- Rasterizer/Rasterizer/Rasterizer.hpp
+++ Rasterizer/Rasterizer/Rasterizer.hpp
@@ -39,7 +39,7 @@ struct Rasterizer {
                 (c * ty - d * tx) * recip,      -(a * ty - b * tx) * recip
             };
         }
-        inline float det() { return a * d - b * c; }
+        inline float det() const { return a * d - b * c; }
         float a, b, c, d, tx, ty;
     };
     struct Bounds {
