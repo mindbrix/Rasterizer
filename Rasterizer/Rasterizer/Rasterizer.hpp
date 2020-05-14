276feeed7b0e3d16e37b9fca44d7c7ae85d2ee86
Nigel Barber <nigel@mindbrix.co.uk>
2020-05-14 19:53:42 +0100

    Geometry::writePoint16() - absorb Point16 conversion

--- Rasterizer/Rasterizer/Rasterizer.hpp
+++ Rasterizer/Rasterizer/Rasterizer.hpp
@@ -86,14 +86,7 @@ struct Rasterizer {
     struct Geometry {
         static float normalizeRadians(float a) { return fmodf(a >= 0.f ? a : (kTau - (fmodf(-a, kTau))), kTau); }
         struct Point16 {
-            Point16(float x0, float y0, uint32_t curve, Bounds& b) {
-                if (x0 == FLT_MAX)
-                    x = y = 0xFFFF;
-                else {
-                    x = uint16_t((x0 - b.lx) / (b.ux - b.lx) * 32767.f) | ((curve & 2) << 14);
-                    y = uint16_t((y0 - b.ly) / (b.uy - b.ly) * 32767.f) | ((curve & 1) << 15);
-                }
-            }
+            Point16(uint16_t x, uint16_t y) : x(x), y(y) {}
             uint16_t x, y;
         };
         enum Type { kNull = 0, kMove, kLine, kQuadratic, kCubic, kClose, kCountSize };
@@ -184,7 +177,12 @@ struct Rasterizer {
             update(kClose, 0, alloc(kClose, 1));
         }
         void writePoint16(float x0, float y0, uint32_t curve) {
-            p16s.emplace_back(Point16(x0, y0, curve, bounds));
+            if (x0 == FLT_MAX)
+                p16s.emplace_back(0xFFFF, 0xFFFF);
+            else
+                p16s.emplace_back(
+                    uint16_t((x0 - bounds.lx) / (bounds.ux - bounds.lx) * 32767.f) | ((curve & 2) << 14),
+                    uint16_t((y0 - bounds.ly) / (bounds.uy - bounds.ly) * 32767.f) | ((curve & 1) << 15));
             if ((p16s.size() - p0) % 4 == 0)
                 pims.emplace_back(im);
         }
