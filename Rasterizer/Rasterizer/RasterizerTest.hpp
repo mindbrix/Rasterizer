c7284d6be8dd11d9b2a6643532cc467382920c6b
Nigel Barber <nigel@mindbrix.co.uk>
2020-05-16 00:12:39 +0100

    RasterizerTest::readEvents() - add rotation animation

--- Rasterizer/Rasterizer/RasterizerTest.hpp
+++ Rasterizer/Rasterizer/RasterizerTest.hpp
@@ -313,12 +313,20 @@ struct RasterizerTest {
     }
     
     bool readEvents(RasterizerState& state) {
-        return concentrichron.scenes.size() || src.scenes.size();// && state.tick & 1;;
+        if (src.pathsCount)
+            for (Ra::Scene *ss = & src.scenes[0], *ds = & dst.scenes[0], *end = ss + src.scenes.size(); ss < end; ss++, ds++)
+                for (int j = 0; j < ss->count; j++) {
+                    Ra::Bounds b = Ra::Bounds(ss->paths[j]->bounds.unit(ss->ctms[j]));
+                    float cx = 0.5f * (b.lx + b.ux), cy = 0.5f * (b.ly + b.uy);
+                    ds->ctms[j] = ss->ctms[j].concat(Ra::Transform::rotation(kTau * (float(state.tick) / 60.f + float(j) / float(ss->count))), cx, cy);
+                }
+            
+        return concentrichron.pathsCount || src.pathsCount;// && state.tick & 1;;
     }
     void writeList(Ra::SceneList& list) {
-        if (concentrichron.scenes.size())
+        if (concentrichron.pathsCount)
             writeConcentrichronList(concentrichron, bounds, list);
-        else if(src.scenes.size())
+        else if(src.pathsCount)
             list.addList(dst);
     }
     size_t refCount = 0;
