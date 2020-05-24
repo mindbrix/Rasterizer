a9dcc24704c3fe0ddabd4224915ef05d9998fd7a
Nigel Barber <nigel@mindbrix.co.uk>
2020-05-24 13:31:23 +0100

    RasterizerState - split out view state

--- Rasterizer/Rasterizer/RasterizerState.hpp
+++ Rasterizer/Rasterizer/RasterizerState.hpp
@@ -117,10 +117,12 @@ struct RasterizerState {
     }
     bool keyDown = false, mouseDown = false, mouseMove = false, useCurves = true;
     double time;
-    float x, y, scale, outlineWidth = 0.f;
+    float x, y;
     int keyCode = 0;
     size_t index = INT_MAX, flags = 0;
     std::vector<Event> events;
+    
+    float scale, outlineWidth = 0.f;
     Ra::Transform ctm, view;
     Ra::Bounds bounds, device;
 };
