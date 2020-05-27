236a5f8fc830a742b717afe7d42812c3449319a4
Nigel Barber <nigel@mindbrix.co.uk>
2020-05-27 18:58:17 +0100

    Reduce kQuadraticFlatness to 1e-2

--- Rasterizer/Rasterizer/Rasterizer.h
+++ Rasterizer/Rasterizer/Rasterizer.h
@@ -10,7 +10,7 @@
 #define kQuadraticScale 0.f
 #define kCubicScale 1e-2f
 #define kCoverScale 2047.9375f
-#define kQuadraticFlatness 1e-3f
+#define kQuadraticFlatness 1e-2f
 #define kMonotoneFlatness 1e-2f
 #define kFatMask 0xFFFFFFF0
 #define kFatHeight 16
