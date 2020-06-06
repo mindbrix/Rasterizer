795164f398d5f88a01be9e703a43ad6d3cbd4401
Nigel Barber <nigel@mindbrix.co.uk>
2020-06-06 14:09:14 +0100

    Cache::Entry - add maxDot

--- Rasterizer/Rasterizer/Rasterizer.hpp
+++ Rasterizer/Rasterizer/Rasterizer.hpp
@@ -265,8 +265,8 @@ struct Rasterizer {
     struct Scene {
         struct Cache {
             struct Entry {
-                Entry(size_t size, bool hasMolecules, bool isFast, float *mols, uint16_t *p16s, uint8_t *p16end) : size(size), hasMolecules(hasMolecules), isFast(isFast), mols(mols), p16s(p16s), p16end(p16end) {}
-                size_t size;  bool hasMolecules;  bool isFast;  float *mols;  uint16_t *p16s;  uint8_t *p16end;
+                Entry(size_t size, bool hasMolecules, float maxDot, bool isFast, float *mols, uint16_t *p16s, uint8_t *p16end) : size(size), hasMolecules(hasMolecules), maxDot(maxDot), isFast(isFast), mols(mols), p16s(p16s), p16end(p16end) {}
+                size_t size;  bool hasMolecules;  float maxDot;  bool isFast;  float *mols;  uint16_t *p16s;  uint8_t *p16end;
             };
             size_t refCount = 0;
             Row<uint32_t> ips;  Row<Entry> entries;
@@ -290,7 +290,7 @@ struct Rasterizer {
                         float w = path->bounds.ux - path->bounds.lx, h = path->bounds.uy - path->bounds.ly, dim = w > h ? w : h;
                         writePath(path.ref, Transform(), Bounds(), true, true, true, path.ref, Geometry::WriteSegment16, writeQuadratic, writeCubic, kQuadraticScale, kCubicScale * (dim > kMoleculesHeight ? 1.f : kMoleculesHeight / dim));
                     }
-                    new (cache->entries.alloc(1)) Cache::Entry(path->p16s.idx, path->molecules.end > 1, path->counts[Geometry::kQuadratic] == 0 && path->counts[Geometry::kCubic] == 0, (float *)path->molecules.base, (uint16_t *)path->p16s.base, path->p16ends.base);
+                    new (cache->entries.alloc(1)) Cache::Entry(path->p16s.idx, path->molecules.end > 1, path->maxDot, path->counts[Geometry::kQuadratic] == 0 && path->counts[Geometry::kCubic] == 0, (float *)path->molecules.base, (uint16_t *)path->p16s.base, path->p16ends.base);
                     *(cache->ips.alloc(1)) = uint32_t(cache->map.size());
                     cache->map.emplace(path->hash(), cache->map.size());
                 }
@@ -489,7 +489,7 @@ struct Rasterizer {
                             if (width == 0.f && clip.uy - clip.ly <= kMoleculesHeight && clip.ux - clip.lx <= kMoleculesHeight) {
                                 ip = scene->cache->ips.base[is], count = scene->cache->entries.base[ip].size / kFastSegments;
                                 gpu.fasts.base[lz + ip] = 1, bounds[iz] = scene->b[is];
-                                bool fast = scene->cache->entries.base[ip].isFast || (clip.uy - clip.ly <= kFastHeight && clip.ux - clip.lx <= kFastHeight);
+                                bool fast = fabsf(m.det()) * scene->cache->entries.base[ip].maxDot < 1.f || (clip.uy - clip.ly <= kFastHeight && clip.ux - clip.lx <= kFastHeight);
                                 GPU::Instance *inst = new (gpu.blends.alloc(1)) GPU::Instance(iz, GPU::Instance::kMolecule | (scene->flags[is] & Scene::kFillEvenOdd ? GPU::Instance::kEvenOdd : 0) | (fast ? GPU::Instance::kFastEdges : 0));
                                  gpu.allocator.allocAndCount(clip.lx, clip.ly, clip.ux, clip.uy, gpu.blends.end - 1, 1, 0, fast ? count : 0, !fast ? count : 0, & inst->quad.cell), inst->quad.cover = 0, inst->quad.iy = int(lz);
                             } else {
