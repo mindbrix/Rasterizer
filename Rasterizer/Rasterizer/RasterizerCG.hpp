43165538f9c5a26478575ce1524633466058685c
Nigel Barber <nigel@mindbrix.co.uk>
2020-06-17 15:21:32 +0100

    RasterizerCG::drawList() - set hairline colours

--- Rasterizer/Rasterizer/RasterizerCG.hpp
+++ Rasterizer/Rasterizer/RasterizerCG.hpp
@@ -36,8 +36,7 @@ struct RasterizerCG {
                     CGContextConcatCTM(ctx, CGFromTransform(t));
                     writePathToCGContext(path, ctx);
                     if (state.outlineWidth || scene.widths[i]) {
-                        if (state.outlineWidth == 0.f)
-                            CGContextSetRGBStrokeColor(ctx, scene.colors[i].r / 255.0, scene.colors[i].g / 255.0, scene.colors[i].b / 255.0, scene.colors[i].a / 255.0);
+                        CGContextSetRGBStrokeColor(ctx, scene.colors[i].r / 255.0, scene.colors[i].g / 255.0, scene.colors[i].b / 255.0, scene.colors[i].a / 255.0);
                         CGContextSetLineWidth(ctx, state.outlineWidth ? (CGFloat)-109.05473e+14 : scene.widths[i] );
                         bool end = scene.flags[i] & Ra::Scene::kOutlineEndCap;
                         bool round = scene.flags[i] & Ra::Scene::kOutlineRounded;
