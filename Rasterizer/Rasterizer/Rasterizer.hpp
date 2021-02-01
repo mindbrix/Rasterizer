dba7faa095db5d66ceebe9ace157621cf2ae2376
Nigel Barber <nigel@mindbrix.co.uk>
2021-02-01 21:42:40 +0000

    Geometry::quadTo() - Use angular flatness test

--- Rasterizer/Rasterizer/Rasterizer.hpp
+++ Rasterizer/Rasterizer/Rasterizer.hpp
@@ -188,7 +188,7 @@ struct Rasterizer {
         }
         void quadTo(float x1, float y1, float x2, float y2) {
             float ax = x2 - x1, ay = y2 - y1, bx = x1 - x0, by = y1 - y0, det = bx * ay - by * ax, dot = ax * bx + ay * by;
-            if (fabsf(det) < 1e-2f) {
+            if (dot * dot / ((ax * ax + ay * ay) * (bx * bx + by * by)) > 0.999695413509548f) {
                 if (dot < 0.f && det)
                     lineTo((x0 + x2) * 0.25f + x1 * 0.5f, (y0 + y2) * 0.25f + y1 * 0.5f);
                 lineTo(x2, y2);
