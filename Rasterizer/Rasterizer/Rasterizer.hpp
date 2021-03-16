7642093fb9f67c8177e23fe6c7e6ebebce448655
Nigel Barber <nigel@mindbrix.co.uk>
2021-03-16 17:48:43 +0000

    splitCubic() - WIP

--- Rasterizer/Rasterizer/Rasterizer.hpp
+++ Rasterizer/Rasterizer/Rasterizer.hpp
@@ -205,10 +205,10 @@ struct Rasterizer {
             bx = 3.f * (x2 - x1), ax = -bx - x0 + x3, by = 3.f * (y2 - y1), ay = -by - y0 + y3;
             if (ax * ax + ay * ay < 1e-2f)
                 quadTo((3.f * (x1 + x2) - x0 - x3) * 0.25f, (3.f * (y1 + y2) - y0 - y3) * 0.25f, x3, y3);
-            else if (x0 == x1 && y0 == y1)
-                quadTo(x2, y2, x3, y3);
-            else if (x2 == x3 && y2 == y3)
-                quadTo(x1, y1, x2, y2);
+//            else if (x0 == x1 && y0 == y1)
+//                quadTo(x2, y2, x3, y3);
+//            else if (x2 == x3 && y2 == y3)
+//                quadTo(x1, y1, x2, y2);
             else {
                 float *pts = points.alloc(6);  pts[0] = x1, pts[1] = y1, pts[2] = x2, pts[3] = y2, pts[4] = x3, pts[5] = y3;
                 update(kCubic, 3, pts);
@@ -729,7 +729,34 @@ struct Rasterizer {
             }
         }
     }
+    static void splitCubic(float x0, float y0, float x1, float y1, float x2, float y2, float x3, float y3, SegmentFunction function, void *info, float precision) {
+        constexpr float multiplier = 10.3923048454; // 18/sqrt(3);
+        float t = 0.f, s, xt, yt, x01, x12, x012, x23, x123, y01, y12, y012, y23, y123, ax, ay, la, cpx, cpy, mx, my;
+        while (t < 1.f) {
+            ax = x3 + 3.f * (x1 - x2) - x0, ay = y3 + 3.f * (y1 - y2) - y0, la = sqrtf(ax * ax + ay * ay);
+            t = la == 0 ? 1.f : 1.f / ceilf(cbrtf(la / (precision * multiplier)));
+            t = t > 1.f ? 1.f : t, s = 1.f - t;
+            x01 = s * x0 + t * x1, y01 = s * y0 + t * y1;
+            x12 = s * x1 + t * x2, y12 = s * y1 + t * y2,
+            x23 = s * x2 + t * x3, y23 = s * y2 + t * y3;
+            x012 = s * x01 + t * x12, y012 = s * y01 + t * y12;
+            x123 = s * x12 + t * x23, y123 = s * y12 + t * y23;
+            xt = s * x012 + t * x123, yt = s * y012 + t * y123;
+            
+            cpx = (3.f * (x012 + x01) - x0 - xt) * 0.25f;
+            cpy = (3.f * (y012 + y01) - y0 - yt) * 0.25f;
+            mx = 0.25f * (x0 + xt) + 0.5f * cpx;
+            my = 0.25f * (y0 + yt) + 0.5f * cpy;
+            
+            (*function)(x0, y0, mx, my, 1, info), (*function)(mx, my, xt, yt, 2, info);
+            
+            x0 = xt, x1 = x123, x2 = x23;
+            y0 = yt, y1 = y123, y2 = y23;
+        }
+    }
     static void divideCubic(float x0, float y0, float x1, float y1, float x2, float y2, float x3, float y3, SegmentFunction function, void *info, float s) {
+        splitCubic(x0, y0, x1, y1, x2, y2, x3, y3, function, info, 0.1f);
+        return;
         float cx, bx, ax, cy, by, ay, a, count, dt, dt2, f3x, f2x, f1x, f3y, f2y, f1y;
         cx = 3.f * (x1 - x0), bx = 3.f * (x2 - x1) - cx, ax = x3 - x0 - cx - bx;
         cy = 3.f * (y1 - y0), by = 3.f * (y2 - y1) - cy, ay = y3 - y0 - cy - by;
