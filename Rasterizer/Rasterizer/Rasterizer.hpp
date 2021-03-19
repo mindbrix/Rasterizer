4cdd2e41f5c592244085d8c645dc9e964e017193
Nigel Barber <nigel@mindbrix.co.uk>
2021-03-19 22:03:37 +0000

    quadTo() - Clarify

--- Rasterizer/Rasterizer/Rasterizer.hpp
+++ Rasterizer/Rasterizer/Rasterizer.hpp
@@ -188,16 +188,16 @@ struct Rasterizer {
         }
         void quadTo(float x1, float y1, float x2, float y2) {
             float ax = x2 - x1, ay = y2 - y1, bx = x1 - x0, by = y1 - y0, adot = ax * ax + ay * ay, bdot = bx * bx + by * by, det = bx * ay - by * ax, dot = ax * bx + ay * by;
-            if (adot == 0.f && bdot == 0.f)
-                return;
-            if (adot == 0.f || bdot == 0.f || dot * dot / (adot * bdot) > 0.999695413509548f) {
-                if (dot < 0.f && det)
-                    lineTo((x0 + x2) * 0.25f + x1 * 0.5f, (y0 + y2) * 0.25f + y1 * 0.5f);
-                lineTo(x2, y2);
-            } else {
-                float *pts = points.alloc(4);  pts[0] = x1, pts[1] = y1, x0 = pts[2] = x2, y0 = pts[3] = y2;
-                update(kQuadratic, 2, pts);
-                ax -= bx, ay -= by, dot = ax * ax + ay * ay, maxDot = maxDot > dot ? maxDot : dot;
+            if (adot != 0.f || bdot != 0.f) {
+                if (adot == 0.f || bdot == 0.f || dot * dot / (adot * bdot) > 0.999695413509548f) {
+                    if (dot < 0.f && det)
+                        lineTo((x0 + x2) * 0.25f + x1 * 0.5f, (y0 + y2) * 0.25f + y1 * 0.5f);
+                    lineTo(x2, y2);
+                } else {
+                    float *pts = points.alloc(4);  pts[0] = x1, pts[1] = y1, x0 = pts[2] = x2, y0 = pts[3] = y2;
+                    update(kQuadratic, 2, pts);
+                    ax -= bx, ay -= by, dot = ax * ax + ay * ay, maxDot = maxDot > dot ? maxDot : dot;
+                }
             }
         }
         void cubicTo(float x1, float y1, float x2, float y2, float x3, float y3) {
