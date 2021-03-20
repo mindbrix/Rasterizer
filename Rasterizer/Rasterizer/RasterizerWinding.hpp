41fe99e70c0b787a39da840dd95bbb931ef1b26a
Nigel Barber <nigel@mindbrix.co.uk>
2021-03-20 15:36:06 +0000

    RasterizerWinding - WindingInfo --> Counter

--- Rasterizer/Rasterizer/RasterizerWinding.hpp
+++ Rasterizer/Rasterizer/RasterizerWinding.hpp
@@ -29,42 +29,42 @@ struct RasterizerWinding {
             }
         return Ra::Range(INT_MAX, INT_MAX);
     }
-    struct WindingInfo {
+    struct Counter {
         float dx, dy, dw;  int winding = 0;
     };
     static void count(float x0, float y0, float x1, float y1, uint32_t curve, void *info) {
-        WindingInfo *w = (WindingInfo *)info;
-        if (w->dy >= (y0 < y1 ? y0 : y1) && w->dy < (y0 > y1 ? y0 : y1)) {
-            float det = (x1 - x0) * (w->dy - y0) - (y1 - y0) * (w->dx - x0);
+        Counter *cntr = (Counter *)info;
+        if (cntr->dy >= (y0 < y1 ? y0 : y1) && cntr->dy < (y0 > y1 ? y0 : y1)) {
+            float det = (x1 - x0) * (cntr->dy - y0) - (y1 - y0) * (cntr->dx - x0);
             if (y0 < y1 && det < 0.f)
-                w->winding++;
+                cntr->winding++;
             else if (y0 > y1 && det > 0.f)
-                w->winding--;
+                cntr->winding--;
         }
     }
     static void countOutline(float x0, float y0, float x1, float y1, uint32_t curve, void *info) {
         if (x0 != x1 || y0 != y1) {
-            WindingInfo *w = (WindingInfo *)info;
+            Counter *cntr = (Counter *)info;
             float ax, ay, bx, by, cx, cy, t, s;
-            ax = x1 - x0, ay = y1 - y0, bx = w->dx - x0, by = w->dy - y0;
+            ax = x1 - x0, ay = y1 - y0, bx = cntr->dx - x0, by = cntr->dy - y0;
             t = (ax * bx + ay * by) / (ax * ax + ay * ay), t = t < 0.f ? 0.f : t > 1.f ? 1.f : t, s = 1.f - t;
-            cx = s * x0 + t * x1 - w->dx, cy = s * y0 + t * y1 - w->dy;
-            if (sqrtf(cx * cx + cy * cy) < 0.5f * w->dw)
-                w->winding = 1;
+            cx = s * x0 + t * x1 - cntr->dx, cy = s * y0 + t * y1 - cntr->dy;
+            if (sqrtf(cx * cx + cy * cy) < 0.5f * cntr->dw)
+                cntr->winding = 1;
         }
     }
     static int pointWinding(Ra::Path& path, Ra::Bounds bounds, Ra::Transform ctm, Ra::Bounds device, float dx, float dy, float w) {
         float ws = sqrtf(fabsf(ctm.det())), uw = w < 0.f ? -w / ws : w;
-        WindingInfo info;  info.dx = dx, info.dy = dy, info.dw = w * (w < 0.f ? -1.f : ws);
+        Counter cntr;  cntr.dx = dx, cntr.dy = dy, cntr.dw = w * (w < 0.f ? -1.f : ws);
         Ra::Transform unit = bounds.inset(-uw, -uw).unit(ctm), inv = unit.invert();
         Ra::Bounds clip = Ra::Bounds(unit).intersect(device);
         float ux = inv.a * dx + inv.c * dy + inv.tx, uy = inv.b * dx + inv.d * dy + inv.ty;
         if (clip.lx != clip.ux && clip.ly != clip.uy && ux >= 0.f && ux < 1.f && uy >= 0.f && uy < 1.f) {
             if (w)
-                Ra::divideGeometry(path.ref, ctm, clip, false, false, false, & info, countOutline, Ra::divideQuadratic, 1.f, Ra::divideCubic, 1.f);
+                Ra::divideGeometry(path.ref, ctm, clip, false, false, false, & cntr, countOutline, Ra::divideQuadratic, 1.f, Ra::divideCubic, 1.f);
             else
-                Ra::divideGeometry(path.ref, ctm, clip, false, true, false, & info, count, Ra::divideQuadratic, 1.f, Ra::divideCubic, 1.f);
+                Ra::divideGeometry(path.ref, ctm, clip, false, true, false, & cntr, count, Ra::divideQuadratic, 1.f, Ra::divideCubic, 1.f);
         }
-        return info.winding;
+        return cntr.winding;
     }
 };
