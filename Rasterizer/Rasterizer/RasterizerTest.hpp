c320422eb66cdd401c13cb962a09a517aaa1d876
Nigel Barber <nigel@mindbrix.co.uk>
2021-04-28 13:49:03 +0100

    addTestScenes() - Add bigRect

--- Rasterizer/Rasterizer/RasterizerTest.hpp
+++ Rasterizer/Rasterizer/RasterizerTest.hpp
@@ -14,10 +14,10 @@ struct RasterizerTest {
     void addTestScenes(Ra::SceneList& list, RaSt& state, Ra::Bounds b, RasterizerFont& font) {
         Ra::Scene scene;
         Ra::Colorant black(0, 0, 0, 64), red(0, 0, 255, 255), alpha64(0, 0, 0, 64);
-        if (1) {
+        if (0) {
             list.empty();
             float uw = 10, dim = 100, grdim = dim + 2 * uw, w, h, s;  size_t sz, grdsz, i;
-            Ra::Path rectPath, closedRectPath, openPath, closedPath, cub0, cub1, cub2, cub3, zed, openTriangle, openCircle, closedCircle, limiter0, twoLines;
+            Ra::Path rectPath, closedRectPath, openPath, closedPath, cub0, cub1, cub2, cub3, zed, openTriangle, openCircle, closedCircle, limiter0, twoLines, bigRect;
             
             rectPath->addBounds(Ra::Bounds(0, 0, dim, dim));
             closedRectPath->addBounds(Ra::Bounds(0, 0, dim, dim));  closedRectPath->close();
@@ -33,8 +33,9 @@ struct RasterizerTest {
             closedCircle->addEllipse(Ra::Bounds(0, 0, dim, dim)), closedCircle->close();
             limiter0->moveTo(0, dim), limiter0->lineTo(0, 0), limiter0->lineTo(0.1 * dim, -0.05 * dim), limiter0->lineTo(dim, 0);
             twoLines->moveTo(0, 0), twoLines->lineTo(0, 0.5 * dim), twoLines->lineTo(0, dim);
+            bigRect->addBounds(Ra::Bounds(0, 0, dim * 1000, dim * 1000)), bigRect->close();
             
-            std::vector<Ra::Path> paths = { rectPath, closedRectPath, openPath, closedPath, cub0, cub1, cub2, cub3, zed, openTriangle, openCircle, closedCircle, limiter0, twoLines };
+            std::vector<Ra::Path> paths = { rectPath, closedRectPath, openPath, closedPath, cub0, cub1, cub2, cub3, zed, openTriangle, openCircle, closedCircle, limiter0, twoLines, bigRect };
             
             for (i = 0; i < 10; i++) {
                 Ra::Path quad;  quad.ref->moveTo(dim, dim), quad.ref->quadTo(-dim + i * 30, dim, 0, 0);
