501abc36f557b61bba1250c90dd4fae3224623c2
Nigel Barber <nigel@mindbrix.co.uk>
2022-03-17 12:34:59 +0000

    RasterizerPDF - Remove skipList[]. Promote sortedIndices[] creation.

--- Rasterizer/Rasterizer/RasterizerPDF.hpp
+++ Rasterizer/Rasterizer/RasterizerPDF.hpp
@@ -202,12 +202,10 @@ struct RasterizerPDF {
         return originCTM.concat(pageCTM);
     }
     
-    static int indexForTextCTM(FS_MATRIX ctm, FPDF_TEXTPAGE text_page, int start, int end, uint8_t *skipList, float *xs, float *ys) {
+    static int indexForTextCTM(FS_MATRIX ctm, FPDF_TEXTPAGE text_page, int start, int end, float *xs, float *ys) {
         int index = -1;
         for (int ch = start; index == -1 && ch < end; ch++)
-            if (skipList[ch])
-                ch += skipList[ch] - 1;
-            else if (ctm.e == xs[ch] && ctm.f == ys[ch])
+            if (ctm.e == xs[ch] && ctm.f == ys[ch])
                 index = ch;
         return index;
     }
@@ -235,8 +233,6 @@ struct RasterizerPDF {
                 char16_t text[4096];
                 char16_t buffer[charCount], *begin = buffer, *dst = begin, *back;
                 bzero(buffer, sizeof(buffer));
-                uint8_t skipList[charCount];
-                bzero(skipList, sizeof(skipList));
                 float xs[charCount], ys[charCount];
                 bzero(xs, sizeof(xs)), bzero(ys, sizeof(xs));
                 
@@ -248,15 +244,24 @@ struct RasterizerPDF {
                 char16_t **textBases = (char16_t **)malloc(objectCount * sizeof(*textBases));
                 int *textIndices = (int *)malloc(objectCount * sizeof(*textIndices));
                 int *textSizes = (int *)malloc(objectCount * sizeof(*textSizes));
+                std::vector<uint64_t> sortedIndices;
+         
+                for (int i = 0; i < objectCount; i++) {
+                    textIndices[i] = -1;
+                    FPDF_PAGEOBJECT pageObject = FPDFPage_GetObject(page, i);
+                    if (FPDFPageObj_GetType(pageObject) == FPDF_PAGEOBJ_TEXT && FPDFPageObj_GetMatrix(pageObject, & m)) {
+                        textIndices[i] = indexForTextCTM(m, text_page, 0, charCount, xs, ys);
+                        if (textIndices[i] != -1)
+                            sortedIndices.emplace_back(textIndices[i]);
+                    }
+                }
+                std::sort(sortedIndices.begin(), sortedIndices.end());
                 
                 for (int i = 0; i < objectCount; i++) {
                     textBases[i] = nullptr;
                     textSizes[i] = 0;
-                    textIndices[i] = -1;
                     FPDF_PAGEOBJECT pageObject = FPDFPage_GetObject(page, i);
-                    if (FPDFPageObj_GetType(pageObject) == FPDF_PAGEOBJ_TEXT && FPDFPageObj_GetMatrix(pageObject, & m)) {
-                        textIndices[i] = indexForTextCTM(m, text_page, 0, charCount, skipList, xs, ys);
-                        
+                    if (FPDFPageObj_GetType(pageObject) == FPDF_PAGEOBJ_TEXT) {
                         unsigned long size0 = FPDFTextObj_GetText(pageObject, text_page, (FPDF_WCHAR *)text, 4096);
                         unsigned long maxCount = charCount - (dst - begin);
                         
@@ -271,11 +276,17 @@ struct RasterizerPDF {
                             assert(size <= maxCount);
                             assert(textIndices[i] != -1);
                             
-                            skipList[textIndices[i]] = size > 255 ? 255 : uint8_t(size);
                             memcpy(dst, text, size * sizeof(*dst));
                             textSizes[i] = int(size);
                             textBases[i] = dst;
                             dst += size;
+                            
+                            uint64_t idx = textIndices[i], space = 0;
+                            auto it = std::find(sortedIndices.begin(), sortedIndices.end(), idx);
+                            if (it < sortedIndices.end()) {
+                                space = (it + 1 < sortedIndices.end() ? it[1] : charCount) - idx;
+                                assert(space >= size);
+                            }
                         }
                     }
                 }
