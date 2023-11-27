0b22de752772bb7ba8d4a4baf9fadf308ea06c99
Nigel Barber <nigel@mindbrix.co.uk>
2023-11-27 21:13:16 +0000

    RasterizerRenderer - Use dispatch_apply()

--- Rasterizer/Rasterizer/RasterizerRenderer.hpp
+++ Rasterizer/Rasterizer/RasterizerRenderer.hpp
@@ -6,38 +6,27 @@
 //  Copyright © 2020 @mindbrix. All rights reserved.
 //
 
-#import "RasterizerQueue.hpp"
 #import "RasterizerState.hpp"
 
 struct RasterizerRenderer {
-     struct ThreadInfo {
-         Ra::Context *context;  Ra::SceneList *list;  Ra::Transform view;
-         Ra::Buffer *buffer;  size_t begin;
-    };
-    static void drawList(void *info) {
-        ThreadInfo *ti = (ThreadInfo *)info;
-        ti->context->drawList(*ti->list, ti->view, ti->buffer);
-    }
-    static void writeContextsToBuffer(void *info) {
-        ThreadInfo *ti = (ThreadInfo *)info;
-        Ra::writeContextToBuffer(*ti->list, ti->context, ti->begin, *ti->buffer);
-    }
-    void renderList(Ra::SceneList& list, RasterizerState& state, Ra::Buffer *buffer) {
+     void renderList(Ra::SceneList& list, RasterizerState& state, Ra::Buffer *buffer) {
         assert(sizeof(uint32_t) == sizeof(Ra::Colorant));
         if (list.pathsCount == 0)
             return;
         buffer->prepare(list), buffer->useCurves = state.useCurves, buffer->fastOutlines = state.fastOutlines;
         size_t i, izs[kQueueCount + 1];  writeIzs(list, izs);
-        ThreadInfo threadInfo[kQueueCount], *ti;
-        for (ti = threadInfo, i = 0; i < kQueueCount; i++, ti++) {
-            ti->list = & list, ti->view = state.view, ti->buffer = buffer;
-            ti->context = contexts + i, ti->context->prepare(state.device, list.pathsCount, izs[i], izs[i + 1]);
+        
+        for (i = 0; i < kQueueCount; i++) {
+            contexts[i].prepare(state.device, list.pathsCount, izs[i], izs[i + 1]);
         }
-        RasterizerQueue::scheduleAndWait(queues, kQueueCount, drawList, threadInfo, sizeof(ThreadInfo), kQueueCount);
+        dispatch_apply(kQueueCount, DISPATCH_APPLY_AUTO, ^(size_t i) {
+            contexts[i].drawList(list, state.view, buffer);
+        });
         size_t begins[kQueueCount], size = Ra::writeContextsToBuffer(list, contexts, kQueueCount, begins, *buffer);
-        for (ti = threadInfo, i = 0; i < kQueueCount; i++, ti++)
-            ti->begin = begins[i];
-        RasterizerQueue::scheduleAndWait(queues, kQueueCount, writeContextsToBuffer, threadInfo, sizeof(ThreadInfo), kQueueCount);
+        size_t *bs = begins;
+        dispatch_apply(kQueueCount, DISPATCH_APPLY_AUTO, ^(size_t i) {
+            Ra::writeContextToBuffer(list, contexts + i, *(bs + i), *buffer);
+        });
         for (int i = 0; i < kQueueCount; i++)
             for (auto entry : contexts[i].entries)
                 *(buffer->entries.alloc(1)) = entry;
@@ -63,7 +52,6 @@ struct RasterizerRenderer {
     static const int kQueueCount = 8;
     void reset() { for (auto& ctx : contexts) ctx.reset(); }
     Ra::Context contexts[kQueueCount];
-    RasterizerQueue queues[kQueueCount];
  };
 
 
