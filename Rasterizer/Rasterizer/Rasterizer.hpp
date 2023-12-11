89e0428807d85633394bc5516a07dd7f1919945f
Nigel Barber <nigel@mindbrix.co.uk>
2023-12-11 11:50:36 +0000

    writeLine() - Remove y = iy * kfh

--- Rasterizer/Rasterizer/Rasterizer.hpp
+++ Rasterizer/Rasterizer/Rasterizer.hpp
@@ -840,7 +840,7 @@ struct Rasterizer {
                 float lx, ux, ly, uy, iy, m, c, y, ny, minx, maxx, scale;
                 lx = fminf(x0, x1), ux = fmaxf(x0, x1);
                 ly = fminf(y0, y1), uy = fmaxf(y0, y1), scale = copysignf(kCoverScale, y1 - y0);
-                iy = floorf(ly * krfh), y = iy * kfh, m = (x1 - x0) / (y1 - y0), c = x0 - m * y0, m *= kfh;
+                iy = floorf(ly * krfh), m = (x1 - x0) / (y1 - y0), c = x0 - m * y0, m *= kfh;
                 minx = (iy + float(m < 0.f)) * m + c;
                 maxx = (iy + float(m > 0.f)) * m + c;
                 for (y = ly; y < uy; y = ny, minx += m, maxx += m, iy++) {
