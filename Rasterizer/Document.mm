4c5300913987cc3eb83eb0e024e640d07f3caa76
Nigel Barber <nigel@mindbrix.co.uk>
2024-08-28 21:46:07 +0100

    Document.mm - Remove superfluous comment

--- Rasterizer/Document.mm
+++ Rasterizer/Document.mm
@@ -30,8 +30,6 @@
 }
 
 - (NSString *)windowNibName {
-    // Override returning the nib file name of the document
-    // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this method and override -makeWindowControllers instead.
     return @"Document";
 }
 
