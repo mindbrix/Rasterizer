d1675559b2c4de2d86de362f236af4e860170b8e
Nigel Barber <nigel@mindbrix.co.uk>
2025-06-21 12:06:02 +0100

    RasterizerRenderer - Renaming

--- Rasterizer/RasterizerRenderer.hpp
+++ Rasterizer/RasterizerRenderer.hpp
@@ -16,31 +16,31 @@ struct RasterizerRenderer {
          buffer->device = device;
          buffer->clip = deviceClip;
          
-        size_t divisions[kQueueCount + 1], *pdivs = divisions;
-        writeDivisions(list, pdivs);
-        dispatch_apply(kQueueCount, DISPATCH_APPLY_AUTO, ^(size_t i) {
+        size_t divisions[kContextCount + 1], *pdivs = divisions;
+        writeBalancedWeightDivisions(list, pdivs);
+        dispatch_apply(kContextCount, DISPATCH_APPLY_AUTO, ^(size_t i) {
             contexts[i].drawList(list, device, deviceClip, view, pdivs[i], pdivs[i + 1], buffer);
         });
-        size_t begins[kQueueCount], *pbegins = begins, size;
-        size = Ra::resizeBuffer(list, contexts, kQueueCount, pbegins, *buffer);
-        dispatch_apply(kQueueCount, DISPATCH_APPLY_AUTO, ^(size_t i) {
+        size_t begins[kContextCount], *pbegins = begins, size;
+        size = Ra::resizeBuffer(list, contexts, kContextCount, pbegins, *buffer);
+        dispatch_apply(kContextCount, DISPATCH_APPLY_AUTO, ^(size_t i) {
             Ra::writeContextToBuffer(list, contexts + i, pbegins[i], *buffer);
         });
-        for (int i = 0; i < kQueueCount; i++)
+        for (int i = 0; i < kContextCount; i++)
             for (auto entry : contexts[i].entries)
                 *(buffer->entries.alloc(1)) = entry;
         size_t end = buffer->entries.end == 0 ? 0 : buffer->entries.back().end;
         assert(size >= end);
     }
     
-    void writeDivisions(Ra::SceneList& list, size_t *divisions) {
+    void writeBalancedWeightDivisions(Ra::SceneList& list, size_t *divisions) {
         size_t total = 0, count, base, i, iz, target;
         for (int j = 0; j < list.scenes.size(); j++)
             total += list.scenes[j].weight;
-        divisions[0] = 0, divisions[kQueueCount] = list.pathsCount;
+        divisions[0] = 0, divisions[kContextCount] = list.pathsCount;
         auto scene = & list.scenes[0];
-        for (count = base = iz = 0, i = 1; i < kQueueCount; i++) {
-            for (target = total * i / kQueueCount; count < target; iz++) {
+        for (count = base = iz = 0, i = 1; i < kContextCount; i++) {
+            for (target = total * i / kContextCount; count < target; iz++) {
                 if (iz - base == scene->count)
                     scene++, base = iz;
                 count += scene->paths->base[iz - base]->types.end;
@@ -48,9 +48,9 @@ struct RasterizerRenderer {
             divisions[i] = iz;
         }
     }
-    static const int kQueueCount = 8;
+    static const int kContextCount = 8;
     void reset() { for (auto& ctx : contexts) ctx.reset(); }
-    Ra::Context contexts[kQueueCount];
+    Ra::Context contexts[kContextCount];
  };
 
 
