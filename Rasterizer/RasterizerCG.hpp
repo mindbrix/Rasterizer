c9a91426ca9de30d02f7af04589b25ad675c9785
Nigel Barber <nigel@mindbrix.co.uk>
2025-07-03 09:17:49 +0100

    RasterizerCG::drawList() - Remove extraneous CGContextSaveGState() call

--- Rasterizer/RasterizerCG.hpp
+++ Rasterizer/RasterizerCG.hpp
@@ -69,7 +69,6 @@ struct RasterizerCG {
             Ra::Transform ctm = list.ctms[j], clip, m;
             CGContextSaveGState(ctx);
             CGContextConcatCTM(ctx, CGFromTransform(ctm));
-            CGContextSaveGState(ctx);
             
             clip = view.concat(Ra::Transform(1e12f, 0.f, 0.f, 1e12f, -5e11f, -5e11f));
             
