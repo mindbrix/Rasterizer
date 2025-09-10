af0ccf022f793579b96e45854d9e66ce0aa677d8
Nigel Barber <nigel@mindbrix.co.uk>
2025-09-10 13:48:03 +0100

    RasterizerWinding::indicesForPoint() - Clarify with mask and inside

--- Rasterizer/src/RasterizerWinding.hpp
+++ Rasterizer/src/RasterizerWinding.hpp
@@ -37,7 +37,9 @@ struct RasterizerWinding {
                     if (inBounds) {
                         int winding = pointWinding(scene.paths->base[si].ptr, scene.bnds.base[si], ctm.concat(scene.ctms->base[si]), px, py, scene.widths->base[si], scene.flags->base[si]);
                         bool even = scene.flags->base[si] & Ra::Scene::kFillEvenOdd;
-                        if ((even && (winding & 1)) || (!even && winding))
+                        int mask = even ? 1 : ~0;
+                        bool inside = (winding & mask) != 0;
+                        if (inside)
                             return Ra::Range(li, si);
                     }
                 }
