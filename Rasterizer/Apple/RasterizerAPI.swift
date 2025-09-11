3a6d102565cb3043cd77198d71767733d87e8c61
Nigel Barber <nigel@mindbrix.co.uk>
2025-09-11 09:24:48 +0100

    SwiftDemo - Rotate around shape center

--- Rasterizer/Apple/RasterizerAPI.swift
+++ Rasterizer/Apple/RasterizerAPI.swift
@@ -29,15 +29,18 @@ class SwiftDemo: NSObject, ListDelegate {
         let time = 0.1 * Date.timeIntervalSinceReferenceDate
         let t = time - floor(time)
         let dim = 100.0
-        let inset = 0.1 * dim
         let rect = CGRect(x: 0, y: 0, width: dim, height: dim)
-        let clip = rect.insetBy(dx: inset, dy: inset)
         let path = RasterizerPath()
+        path.add(rect)
         path.addEllipse(rect)
         
+        let tx = rect.midX
+        let ty = rect.midY
+        let rotation = CGAffineTransform(rotationAngle: t * 2 * Double.pi)
+        let ctm = CGAffineTransform(translationX: -tx, y: -ty).concatenating(rotation).concatenating(CGAffineTransform(translationX: tx, y: ty))
         let color = CGColor(red: 1, green: 0, blue: 0, alpha: 1)
         let scene = RasterizerScene()
-        scene.add(path, ctm: CGAffineTransform(rotationAngle: t * 2 * Double.pi), color: color, width: 0, flags: 0, clip: clip)
+        scene.add(path, ctm: ctm, color: color, width: 0, flags: SceneFlags.fillEvenOdd.rawValue)
         
         let list = RasterizerSceneList()
         list.add(scene, ctm: .identity)
