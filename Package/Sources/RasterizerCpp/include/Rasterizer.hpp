b86e955d5619ed3311de7a636b08af56d534ed1f
Nigel Barber <nigel@mindbrix.co.uk>
2025-12-08 20:42:32 +0000

    P16Outliner::miter() - Compact

--- Package/Sources/RasterizerCpp/include/Rasterizer.hpp
+++ Package/Sources/RasterizerCpp/include/Rasterizer.hpp
@@ -1612,14 +1612,12 @@ struct Rasterizer {
             prevx = prevy = FLT_MAX;
         }
         void miter(float px, float py, float x, float y, float nx, float ny) {
-            float ax, ay, bx, by, ra, rb, tx, ty, rt, invcos, len, mx, my;
+            float ax, ay, bx, by, ra, rb, tx, ty, rt, invcos, len;
             ax = x - px, ay = y - py, ra = 1.f / sqrtf(ax * ax + ay * ay + FLT_EPSILON), ax *= ra, ay *= ra;
             bx = nx - x, by = ny - y, rb = 1.f / sqrtf(bx * bx + by * by + FLT_EPSILON), bx *= rb, by *= rb;
             tx = ax + bx, ty = ay + by, rt = 1.f / sqrtf(tx * tx + ty * ty + FLT_EPSILON), tx *= rt, ty *= rt;
-            invcos = 1.f / fabsf(dx * tx + dy * ty);
-            len = kMoleculesRange / kMiterRange * fminf(kMiterRange, invcos);
-            mx = -len * ty, my = len * tx;
-            new (miters->alloc(1)) Vector16(mx, my);
+            invcos = 1.f / fabsf(dx * tx + dy * ty), len = kMoleculesRange / kMiterRange * fminf(kMiterRange, invcos);
+            new (miters->alloc(1)) Vector16(-len * ty, len * tx);
             if (invcos > kMiterLimit)
                 miters->back().x |= Vector16::kIsCap;
         }
