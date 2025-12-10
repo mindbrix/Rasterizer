18e20e2547d075da891d0a84ae8145d2775a2316
Nigel Barber <nigel@mindbrix.co.uk>
2025-12-10 21:46:51 +0000

    Remove stale pdfium include paths

--- Package/Package.swift
+++ Package/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "RasterizerSwift",
     platforms: [
-        .macOS(.v14)
+        .macOS(.v12)
         ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
