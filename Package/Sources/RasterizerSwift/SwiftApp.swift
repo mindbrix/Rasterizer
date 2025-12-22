b1eb5e05d76b6fbdba2ddf4cfae300635619e134
Nigel Barber <nigel@mindbrix.co.uk>
2025-12-22 16:58:37 +0000

    SwiftApp - Absorb algorithms from data structures

--- Package/Sources/RasterizerSwift/SwiftApp.swift
+++ Package/Sources/RasterizerSwift/SwiftApp.swift
@@ -59,6 +59,19 @@ enum Control {
     case button(label: Label, closure: Closure)
     case label(label: Label)
     case text(label: Label, key: Store.Key)
+    
+    var isTappable: Bool {
+        closure != nil
+    }
+    var closure: Closure? {
+        switch self {
+        case .button(_, let closure):
+            return closure
+        default:
+            break
+        }
+        return nil
+    }
 }
 
 struct Page {
@@ -85,42 +98,63 @@ struct SetRun {
     let control: Control
 }
 
-extension Control {
+class SwiftApp {
     enum Alignment: Double {
         case min = 0, mid = 0.5, max = 1
     }
-    func originIn(_ bounds: CGRect, runs: [Run], alignx: Alignment, aligny: Alignment) -> CGPoint {
-        let width = runs.reduce(0.0) { result, run in
-            result + run.bounds.width
+    protocol Delegate: AnyObject {
+        func controlsFor(_ pageID: PageID) -> [Control]?
+    }
+    weak var delegate: Delegate?
+    var pageID = PageID.Null
+    var font = Font(name: "HelveticaNeue-Medium", size: 72)
+    let store = Store()
+    
+    func drawIn(_ bounds: CGRect, scene: RAScene) {
+        guard pageID != .Null, let delegate, let controls = delegate.controlsFor(pageID) else {
+            return
         }
-        let height = runs.reduce(0.0) { result, run in
-            max(result, run.bounds.maxY)
+        CGRect.drawGridIn(bounds, count: controls.count, scene: scene)
+        for setrun in setRunsIn(bounds, controls: controls) {
+            let ctm = CGAffineTransform(translationX: setrun.origin.x, y: setrun.origin.y)
+            scene.addRect(setrun.run.bounds, ctm: ctm, width: 1, color: RAPaint())
+            scene.addTextLine(setrun.run.attributedString, ctm: ctm, clip: .zero)
         }
-        let tx = alignx.rawValue * (bounds.width - width)
-        let ty = aligny.rawValue * (bounds.height - height)
-        
-        return CGPoint(x: bounds.minX + tx, y: bounds.minY + ty)
+    }
+    func mouseDownIn(_ bounds: CGRect, mx: Double, my: Double) {
+        guard pageID != .Null, let delegate, let page = delegate.controlsFor(pageID) else {
+            return
+        }
+        for setrun in setRunsIn(bounds, controls: page).filter({ $0.control.isTappable }).reversed() {
+            if setrun.run.bounds.contains(CGPoint(x: mx - setrun.origin.x, y: my - setrun.origin.y)) {
+                setrun.control.closure?(self)
+                break
+            }
+        }
     }
     
-    var isTappable: Bool {
-        closure != nil
-    }
-    var closure: Closure? {
-        switch self {
-        case .button(_, let closure):
-            return closure
-        default:
-            break
-        }
-        return nil
+    func setRunsIn(_ bounds: CGRect, controls: [Control]) -> [SetRun] {
+        return controls.enumerated().flatMap({ i, control in
+            let b = CGRect.boundsForIndex(bounds, index: i, count: controls.count)
+            let runs = runsFor(control: control)
+            let origin = originIn(b, runs: runs, alignx: .mid, aligny: .mid)
+
+            var setruns: [SetRun] = []
+            var tx = 0.0
+            for run in runs {
+                setruns.append(SetRun(run: run, origin: CGPoint(x: tx + origin.x, y: origin.y), control: control))
+                tx += run.bounds.width
+            }
+            return setruns
+        })
     }
     
-    func runsFor(font: Font, store: Store, pageID: PageID) -> [Run] {
+    func runsFor(control: Control) -> [Run] {
         let red = RAPaint(red: 1, green: 0, blue: 0, alpha: 1)
         let black = RAPaint()
         let gray = RAPaint(gray: 0.66, alpha: 1)
         
-        switch self {
+        switch control {
         case .button(let label, _):
             return [
                 Run(string: label.rawValue, font: font, color: red)
@@ -136,54 +170,17 @@ extension Control {
             ]
         }
     }
-}
-
-
-class SwiftApp {
-    protocol Delegate: AnyObject {
-        func controlsFor(_ pageID: PageID) -> [Control]?
-    }
-    weak var delegate: Delegate?
-    var pageID = PageID.Null
-    var font = Font(name: "HelveticaNeue-Medium", size: 72)
-    let store = Store()
     
-    func drawIn(_ bounds: CGRect, scene: RAScene) {
-        guard pageID != .Null, let delegate, let controls = delegate.controlsFor(pageID) else {
-            return
+    func originIn(_ bounds: CGRect, runs: [Run], alignx: Alignment, aligny: Alignment) -> CGPoint {
+        let width = runs.reduce(0.0) { result, run in
+            result + run.bounds.width
         }
-        CGRect.drawGridIn(bounds, count: controls.count, scene: scene)
-        for setrun in setRunsIn(bounds, page: controls) {
-            let ctm = CGAffineTransform(translationX: setrun.origin.x, y: setrun.origin.y)
-            scene.addRect(setrun.run.bounds, ctm: ctm, width: 1, color: RAPaint())
-            scene.addTextLine(setrun.run.attributedString, ctm: ctm, clip: .zero)
+        let height = runs.reduce(0.0) { result, run in
+            max(result, run.bounds.maxY)
         }
-    }
-    func mouseDownIn(_ bounds: CGRect, mx: Double, my: Double) {
-        guard pageID != .Null, let delegate, let page = delegate.controlsFor(pageID) else {
-            return
-        }
-        for setrun in setRunsIn(bounds, page: page).filter({ $0.control.isTappable }).reversed() {
-            if setrun.run.bounds.contains(CGPoint(x: mx - setrun.origin.x, y: my - setrun.origin.y)) {
-                setrun.control.closure?(self)
-                break
-            }
-        }
-    }
-    
-    func setRunsIn(_ bounds: CGRect, page: [Control]) -> [SetRun] {
-        return page.enumerated().flatMap({ i, control in
-            let b = CGRect.boundsForIndex(bounds, index: i, count: page.count)
-            let runs = control.runsFor(font: font, store: store, pageID: pageID)
-            let origin = control.originIn(b, runs: runs, alignx: .mid, aligny: .mid)
-
-            var setruns: [SetRun] = []
-            var tx = 0.0
-            for run in runs {
-                setruns.append(SetRun(run: run, origin: CGPoint(x: tx + origin.x, y: origin.y), control: control))
-                tx += run.bounds.width
-            }
-            return setruns
-        })
+        let tx = alignx.rawValue * (bounds.width - width)
+        let ty = aligny.rawValue * (bounds.height - height)
+        
+        return CGPoint(x: bounds.minX + tx, y: bounds.minY + ty)
     }
 }
