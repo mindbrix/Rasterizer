b78f141d7f7a53a7318aec1c9ff46a254007bc3a
Nigel Timothy Barber <nigel@mindbrix.co.uk>
2026-01-18 08:42:01 +0000

    Package - Add iOS

--- Package/Package.swift
+++ Package/Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "RasterizerSwift",
     platforms: [
-        .macOS(.v12)
+        .macOS(.v12),
+        .iOS(.v13)
         ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
