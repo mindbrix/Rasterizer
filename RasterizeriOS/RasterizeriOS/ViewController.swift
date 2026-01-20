ff8491d9d991202fc352344351219893df6db183
Nigel Timothy Barber <nigel@mindbrix.co.uk>
2026-01-20 20:54:44 +0000

    ViewController - Add pan, pin & rotate gestures

--- RasterizeriOS/RasterizeriOS/ViewController.swift
+++ RasterizeriOS/RasterizeriOS/ViewController.swift
@@ -9,20 +9,73 @@ import UIKit
 import RasterizerObjC
 import RasterizerSwift
 
-class ViewController: UIViewController, RASceneListDelegate {
-    func shouldRedraw(atTime time: Double, width: Double, height: Double) -> Bool {
-        true
-    }
+class ViewController: UIViewController {
+    var ctm = CGAffineTransform.identity
+    var down = CGAffineTransform.identity
     
-    func getListAtTime(_ time: Double, width: Double, height: Double) -> RASceneList {
-        CounterRotatingCircles(time, width: width, height: height)
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if let view = self.view as? RasterizerView {
             view.listDelegate = self
+            view.isUserInteractionEnabled = true
+            view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(wasPanned)))
+            view.addGestureRecognizer(UIPinchGestureRecognizer(target: self, action: #selector(wasPinched)))
+            view.addGestureRecognizer(UIRotationGestureRecognizer(target: self, action: #selector(wasRotated)))
         }
+    }
+    
+    @objc func wasPanned(_ recognizer: UIPanGestureRecognizer) {
+        switch recognizer.state {
+        case .began:
+            down = ctm
+        case .changed:
+            let tx = recognizer.translation(in: self.view)
+            ctm = down
+            ctm.tx += tx.x
+            ctm.ty -= tx.y
+        default:
+            break
+        }
+    }
+    
+    @objc func wasPinched(_ recognizer: UIPinchGestureRecognizer) {
+        switch recognizer.state {
+        case .began:
+            down = ctm
+        case .changed:
+            let s = recognizer.scale
+            let cx = self.view.bounds.midX
+            let cy = self.view.bounds.midY
+            ctm = down.concatAroundCenter(t: CGAffineTransform(scaleX: s, y: s), cx: cx, cy: cy)
+        default:
+            break
+        }
+    }
+    
+    @objc func wasRotated(_ recognizer: UIRotationGestureRecognizer) {
+        switch recognizer.state {
+        case .began:
+            down = ctm
+        case .changed:
+            let r = recognizer.rotation
+            let cx = self.view.bounds.midX
+            let cy = self.view.bounds.midY
+            ctm = down.concatAroundCenter(t: CGAffineTransform(rotationAngle: -r), cx: cx, cy: cy)
+        default:
+            break
+        }
+    }
+}
+
+extension ViewController: RASceneListDelegate {
+    func shouldRedraw(atTime time: Double, width: Double, height: Double) -> Bool {
+        true
+    }
+    
+    func getListAtTime(_ time: Double, width: Double, height: Double) -> RASceneList {
+        let list = CounterRotatingCircles(time, width: width, height: height)
+        list.ctm = ctm
+        return list
     }
 }
--- SwiftDemo/SwiftDemo/src/SwiftDemo.swift
+++ SwiftDemo/SwiftDemo/src/SwiftDemo.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import RasterizerObjC
+import RasterizerSwift
 
 
 class SwiftDemo: NSObject {
