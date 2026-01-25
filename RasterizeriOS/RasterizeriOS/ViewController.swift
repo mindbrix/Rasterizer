60dd238178e0e79d3567b800e1a726a1113f5ffe
Nigel Timothy Barber <nigel@mindbrix.co.uk>
2026-01-25 11:38:38 +0000

    ViewController - More SVGs

--- RasterizeriOS/RasterizeriOS/ViewController.swift
+++ RasterizeriOS/RasterizeriOS/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     var down = CGAffineTransform.identity
     var redraw = false
     var svgIndex = 0
-    let svgNames = ["Anime_Girl", "AntigenicShift_HiRes", "contour", "hawaii", "Manchester_Union_Democrat_office_1877", "PToT_hi-res_source_nobackground", "reschart", "tiger"]
+    let svgNames = ["Anime_Girl", "AntigenicShift_HiRes", "car", "contour", "drops", "hawaii",  "Manchester_Union_Democrat_office_1877", "paris-30k", "PToT_hi-res_source_nobackground", "reschart", "Sun_poster", "tiger"]
     var svgList: RASceneList? {
         didSet {
             if let svgList {
